a34501936a1b864288e840291589acd735489646
linoal <1321932+linoal@users.noreply.github.com>
2022-04-06 13:47:12 +0900

    テストコード整理

--- Tests/EditModeTests/TestsFileConverter/TestFilePathValidator.cs
+++ Tests/EditModeTests/TestsFileConverter/TestFilePathValidator.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using System.IO;
 using NUnit.Framework;
 using UnityEngine.TestTools;
 using PlateauUnitySDK.Editor.FileConverter;
-using UnityEditor.VersionControl;
 
 namespace PlateauUnitySDK.Tests.EditModeTests.TestsFileConverter {
     
@@ -16,16 +14,11 @@ namespace PlateauUnitySDK.Tests.EditModeTests.TestsFileConverter {
         // 実在するファイルが与えられたときのテストケース
         #if UNITY_STANDALONE_WIN
         // このコードを動かしているWindows PCであればおそらく存在するであろうファイルのパスを例にとり、存在するファイルが与えられたときに有効判定が出ることをチェックします。
-        [TestCase("C:\\Program Files\\Unity\\Hub\\Editor\\2020.3.32f1\\Editor\\Unity.exe", "exe", true)]
+        [TestCase("C:\\Windows\\System32\\input.dll", "dll", true)]
         // 拡張子が合わないときに false になることもチェックします。
-        [TestCase("C:\\Program Files\\Unity\\Hub\\Editor\\2020.3.32f1\\Editor\\Unity.exe", "wrongExtension", false)]
-        
-        // Windows以外のOSをお使いの方は、お手数ですがここに次のような記述を追加してください:
-        // #elif そのOS固有のキーワード
-        // [TestCase("そのOSの利用者はみんな持っているであろうファイルのパス.何かの拡張子", "何かの拡張子", true]
-        // [TestCase("同上.何かの拡張子", "合わない拡張子", false]
+        [TestCase("C:\\Windows\\System32\\input.dll", "wrongExtension", false)]
         #else
-        このコードは処理されないはずです
+        [Ignore("Windows以外での実行には対応していないテストです")]
         #endif
         
         // 実在しないファイルが与えられたときのテストケース
@@ -43,17 +36,13 @@ namespace PlateauUnitySDK.Tests.EditModeTests.TestsFileConverter {
         
         // 実在するファイルが与えられたときのテストケース
         #if UNITY_STANDALONE_WIN
-        // このコードを動かしているWindows PCであればおそらく存在するであろうフォルダを例にとり、存在するフォルダが与えられたときに有効判定が出ることをチェックします。
+        // このコードを動かしているWindows PCであればおそらく存在するであろうフォルダ(Program Files)に例にとり、
+        // 存在するフォルダでの新規ファイル作成(fbx)を想定したときに有効判定が出ることをチェックします。
         [TestCase("C:\\Program Files\\User_wants_to_save_here.fbx", "fbx", true)]
         // 拡張子が合わないときに false になることもチェックします。
         [TestCase("C:\\Program Files\\User_wants_to_save_here.fbx", "wrongExtension", false)]
-        
-        // Windows以外のOSをお使いの方は、お手数ですがここに次のような記述を追加してください:
-        // #elif そのOS固有のキーワード
-        // [TestCase("そのOSの利用者はみんな持っているであろうフォルダのパス/foo.何かの拡張子", "何かの拡張子", true]
-        // [TestCase("同上/foo.何かの拡張子", "合わない拡張子", false]
         #else
-        このコードは処理されないはずです
+        [Ignore("Windows以外での実行には対応していないテストです")]
         #endif
         
         // 実在しないファイルが与えられたときのテストケース
@@ -93,8 +82,8 @@ namespace PlateauUnitySDK.Tests.EditModeTests.TestsFileConverter {
             FilePathValidator.TestOnly_SetUnityProjectDataPath(prevDataPath);
         }
 
-
-        [Test] // Assetsフォルダ以下は未対応なので例外を出すことを確認します。
+        // フルパスからアセットパス変換で、Assetsフォルダの外が指定されたときに例外を出すことを確認します。
+        [Test] 
         public void Test_FullPathToAssetsPath_Error() {
             Assert.That(()=> {
                     FilePathValidator.FullPathToAssetsPath("C:\\dummy\\OutsideAssets\\a.fbx");
