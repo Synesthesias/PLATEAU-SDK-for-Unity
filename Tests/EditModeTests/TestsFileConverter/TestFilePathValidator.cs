0843e022e299a110110c884a48895c67eb60147d
linoal <1321932+linoal@users.noreply.github.com>
2022-05-30 14:56:19 +0900

    TestFilePathValidatorのコードを整理

--- Tests/EditModeTests/TestsFileConverter/TestFilePathValidator.cs
+++ Tests/EditModeTests/TestsFileConverter/TestFilePathValidator.cs
@@ -8,87 +8,128 @@ namespace PlateauUnitySDK.Tests.EditModeTests.TestsFileConverter {
     
     [TestFixture]
     public class TestFilePathValidator {
+        
+        
+        [TestCaseSource(nameof(testIsValidInputFilePath))]
+        public bool Test_IsValidInputFilePath(string filePath, string extension) {
+            LogAssert.ignoreFailingMessages = true;
+            bool result = FilePathValidator.IsValidInputFilePath(filePath, extension, false);
+            return result;
+        }
 
-        /// <summary>
-        /// 入力ファイル用のパスとして正しいかどうかを判定できるか確認します。
-        /// </summary>
         
-        // 実在するファイルが与えられたときのテストケース
-        #if UNITY_STANDALONE_WIN
-        // このコードを動かしているWindows PCであればおそらく存在するであろうファイルのパスを例にとり、存在するファイルが与えられたときに有効判定が出ることをチェックします。
-        [TestCase("C:\\Windows\\System32\\input.dll", "dll", true)]
-        // 拡張子が合わないときに false になることもチェックします。
-        [TestCase("C:\\Windows\\System32\\input.dll", "wrongExtension", false)]
-        #else
-        // Linuxで上と同様のことをテストします。
-        [TestCase("/etc/issue.net", "net", true)]
-        [TestCase("/etc/issue.net", "wrongExtension", false)]
-        #endif
+        private static TestCaseData[] testIsValidInputFilePath =
+        {
+            #if UNITY_STANDALONE_WIN
+            
+            new TestCaseData("C:\\Windows\\System32\\input.dll", "dll")
+                .Returns(true)
+                .SetName("[Windowsのみ] 実在するファイルパスは true になります。"),
+            
+            new TestCaseData("C:\\Windows\\System32\\input.dll", "間違い拡張子")
+                .Returns(false)
+                .SetName("[Windowsのみ] 実在するファイルであっても拡張子が違うと false になります。"),
+            
+            #else
+            
+            new TestCaseData("/etc/issue.net", "net")
+                .Returns(true)
+                .SetName("[Linuxのみ] 実在するファイルパスは true になります"),
+            
+            new TestCaseData("C:\\Windows\\System32\\input.dll", "wrongExtension")
+                .Returns(false)
+                .SetName("[Linuxのみ] 実在するファイルであっても拡張子が違うと false になります"),
+            
+            #endif
+            
+            new TestCaseData("/存在しない/ファイルパス/ダミー.fbx", "fbx")
+                .Returns(false)
+                .SetName("実在しないファイルパスは false になります。")
+
+        };
         
-        // 実在しないファイルが与えられたときのテストケース
-        [TestCase("/NotFound/Dummy/Missing.fbx", "fbx", false)]
-        public void Test_IsValidInputFilePath(string filePath, string extension, bool expected) {
+        
+        
+        [TestCaseSource(nameof(testIsValidOutputPath))]
+        public bool Test_IsValidOutputFilePath(string filePath, string extension) {
             LogAssert.ignoreFailingMessages = true;
-            Assert.AreEqual(FilePathValidator.IsValidInputFilePath(filePath, extension, false), expected);
+            bool result = FilePathValidator.IsValidOutputFilePath(filePath, extension);
+            return result;
         }
+
+
+        private static TestCaseData[] testIsValidOutputPath =
+        {
+#if UNITY_STANDALONE_WIN
+
+            new TestCaseData("C:\\Program Files\\User_wants_to_save_here.fbx", "fbx")
+                .Returns(true)
+                .SetName("[Windowsのみ] 実在するフォルダ上で新規のファイル名を指定した時に true になります。"),
+
+            new TestCaseData("C:\\Program Files\\User_wants_to_save_here.fbx", "間違い拡張子")
+                .Returns(false)
+                .SetName("[Windowsのみ] 実在するフォルダであっても、拡張子が違うと false になります。"),
+
+#else
+
+            new TestCaseData("/etc/User_wants_to_save_here.fbx", "fbx")
+                .Returns(true)
+                .SetName("[Linuxのみ] 実在するフォルダ上で新規のファイル名を指定した時に true になります"),
+
+            new TestCaseData("/etc/User_wants_to_save_here.fbx", "間違い拡張子")
+                .Returns(false)
+                .SetName("[Linuxのみ] 実在するフォルダであっても、拡張子が違うと false になります"),
+
+#endif
+
+            new TestCaseData("/存在しないフォルダ/ダミー/新規ファイル.fbx", "fbx")
+                .Returns(false)
+                .SetName("実在しないフォルダでは false になります。")
+
+        };
         
         
         
-        /// <summary>
-        /// 出力ファイル用のパスとして正しいかどうかを判定できるか確認します。
-        /// </summary>
-        
-        // 実在するファイルが与えられたときのテストケース
-        #if UNITY_STANDALONE_WIN
-        // このコードを動かしているWindows PCであればおそらく存在するであろうフォルダ(Program Files)に例にとり、
-        // 存在するフォルダでの新規ファイル作成(fbx)を想定したときに有効判定が出ることをチェックします。
-        [TestCase("C:\\Program Files\\User_wants_to_save_here.fbx", "fbx", true)]
-        // 拡張子が合わないときに false になることもチェックします。
-        [TestCase("C:\\Program Files\\User_wants_to_save_here.fbx", "wrongExtension", false)]
-        #else
-        // Linuxで上と同様のことをテストします。
-        [TestCase("/etc/User_wants_to_save_here.fbx", "fbx", true)]
-        [TestCase("/etc/User_wants_to_save_here.fbx", "wrongExtension", false)]
-        #endif
-        
-        // 実在しないファイルが与えられたときのテストケース
-        [TestCase("/NotFound/Dummy/Missing.fbx", "fbx", false)]
-        public void Test_IsValidOutputFilePath(string filePath, string extension, bool expected) {
-            LogAssert.ignoreFailingMessages = true;
-            Assert.AreEqual(FilePathValidator.IsValidOutputFilePath(filePath, extension), expected);
-        }
-        
-        
-        
-        /// <summary>
-        /// フルパスから Assets で始まるパスへの変換ができることを確認します。
-        /// プロジェクトのAssetsフォルダが assetsDir にあると仮定して、 フルパス を変換したら expected になることを確認します。
-        /// </summary>
-        
-        // Windowsのパス表記への対応をチェックします。
-        #if UNITY_STANDALONE_WIN
-        [TestCase("C:/DummyUnityProjects/Assets", "C:\\DummyUnityProjects\\Assets\\FooBar\\FooBarModelFile.fbx", "Assets/FooBar/FooBarModelFile.fbx")]
-        #endif
-        // Linuxのパス表記への対応をチェックします。
-        [TestCase("/home/linuxUser/DummyUnityProjects/Assets", "/home/linuxUser/DummyUnityProjects/Assets/foobar.obj", "Assets/foobar.obj")]
-        // 紛らわしい名前への対応をチェックします。
-        [TestCase("Assets/Assets", "Assets/Assets/Assets/Assets", "Assets/Assets/Assets")]
-        // 日本語名、絵文字、スペースへの対応をチェックします。
-        [TestCase("C:/日本語話者の プロジェクト♪🎶/Assets", "C:/日本語話者の プロジェクト♪🎶/Assets/♪ 🎶.wav", "Assets/♪ 🎶.wav" )]
-        
-        public void Test_FullPathToAssetsPath_Normal(string assetsDir, string fullPath, string expectedAssetsPath) {
+        [TestCaseSource(nameof(testFullPathToAssetsPathNormal))]
+        public string Test_FullPathToAssetsPath_Normal(string assetsDir, string fullPath) {
             // 後でAssetsフォルダのパス設定を戻すために覚えておきます。
             string prevDataPath = ReflectionUtil.GetPrivateStaticFieldVal<string>(typeof(FilePathValidator), "unityProjectDataPath");
             
-            // Assetsフォルダがこのような場所にあると仮定します。
+            // Assetsフォルダが引数のパスにあると仮定します。
             ReflectionUtil.SetPrivateStaticFieldVal(assetsDir, typeof(FilePathValidator), "unityProjectDataPath");
             
-            // テストケースをチェックします。 
-            Assert.AreEqual(expectedAssetsPath, FilePathValidator.FullPathToAssetsPath(fullPath));
+            // テスト対象コードを実行します。 
+            string result = FilePathValidator.FullPathToAssetsPath(fullPath);
             
             // Assetsフォルダの設定を戻します。
             ReflectionUtil.SetPrivateStaticFieldVal(prevDataPath, typeof(FilePathValidator), "unityProjectDataPath");
+            return result;
         }
+
+        private static TestCaseData[] testFullPathToAssetsPathNormal =
+        {
+#if UNITY_STANDALONE_WIN
+
+            new TestCaseData("C:/DummyUnityProjects/Assets",
+                    "C:\\DummyUnityProjects\\Assets\\FooBar\\FooBarModelFile.fbx")
+                .Returns("Assets/FooBar/FooBarModelFile.fbx")
+                .SetName("[Windowsのみ] バックスラッシュ区切りのフルパスから、Assetsで始まるパスへ変換します。"),
+
+#endif
+
+            new TestCaseData("/home/linuxUser/DummyUnityProjects/Assets",
+                    "/home/linuxUser/DummyUnityProjects/Assets/foobar.obj")
+                .Returns("Assets/foobar.obj")
+                .SetName("Linux形式のフルパスから、Assetsで始まるパスへ変換します。"),
+
+            new TestCaseData("Assets/Assets", "Assets/Assets/Assets/Assets")
+                .Returns("Assets/Assets/Assets")
+                .SetName("まぎらわしい名前のパスに対応します。"),
+
+            new TestCaseData("C:/日本語話者の プロジェクト♪🎶/Assets", "C:/日本語話者の プロジェクト♪🎶/Assets/♪ 🎶.wav")
+                .Returns("Assets/♪ 🎶.wav")
+                .SetName("絵文字やスペースが含まれるパスに対応します。")
+        };
         
         [Test] 
         public void FullPathToAssetsPath_Returns_Error_When_Outside_Assets_Folder() {
