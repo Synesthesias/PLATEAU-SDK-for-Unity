6a481500b194c9f6bf6e39bcfd64bbe31d6b7968
linoal <1321932+linoal@users.noreply.github.com>
2022-06-10 17:48:21 +0900

    obj変換してもメッシュがなければ変換後ファイルを削除して変換失敗とする機能を実装(Assetsフォルダ内のみ)

--- Editor/FileConverter/Converters/GmlToObjFileConverter.cs
+++ Editor/FileConverter/Converters/GmlToObjFileConverter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using LibPLATEAU.NET.CityGML;
 using PlateauUnitySDK.Runtime.Util;
@@ -13,6 +14,9 @@ namespace PlateauUnitySDK.Editor.FileConverter.Converters
     /// 使い方は <see cref="SetConfig"/> してから <see cref="Convert"/> を呼んで <see cref="Dispose"/> します。
     /// <see cref="Dispose"/> を忘れると、変換後もファイルが使用中となり外部から変更できなくなります。
     /// usingステートメントを使うことで暗黙的に <see cref="Dispose"/> を呼ぶことができます。
+    ///
+    /// 変換後のobjファイルパスが Assets フォルダ内である場合に限り、変換時に次の機能が働きます:
+    /// 3Dメッシュの数を確認して数が 0 であればそのobjファイルを削除し変換失敗とします。
     /// </summary>
     public class GmlToObjFileConverter : IFileConverter, IDisposable
     {
@@ -20,8 +24,6 @@ namespace PlateauUnitySDK.Editor.FileConverter.Converters
         private readonly ObjWriter objWriter;
 
         private CitygmlParserParams gmlParserParams;
-        // private MeshGranularity meshGranularity;
-        // private AxesConversion axesConversion;
         private DllLogLevel logLevel;
 
         private int disposed;
@@ -43,14 +45,15 @@ namespace PlateauUnitySDK.Editor.FileConverter.Converters
         /// <param name="meshGranularity">メッシュのオブジェクト分けの粒度です。</param>
         /// <param name="axesConversion">座標軸の向きです。Unityの場合は通常RUFです。</param>
         /// <param name="optimizeFlg">trueのとき最適化します。</param>
+        /// <param name="logLevelArg">これ未満のレベルのログは表示しません。</param>
         public void SetConfig(MeshGranularity meshGranularity, AxesConversion axesConversion = AxesConversion.RUF,
-            bool optimizeFlg = true, DllLogLevel logLevel = DllLogLevel.Error)
+            bool optimizeFlg = true, DllLogLevel logLevelArg = DllLogLevel.Error)
         {
             this.gmlParserParams.Optimize = optimizeFlg;
             this.gmlParserParams.Tessellate = true; // true でないと、頂点の代わりに LinearRing が生成されてしまい 3Dモデルには不適になります。
             this.objWriter.SetMeshGranularity(meshGranularity);
             this.objWriter.SetDestAxes(axesConversion);
-            this.objWriter.GetDllLogger().SetLogLevel(logLevel);
+            this.objWriter.GetDllLogger().SetLogLevel(logLevelArg);
         }
 
         /// <summary>
@@ -107,15 +110,30 @@ namespace PlateauUnitySDK.Editor.FileConverter.Converters
                     AssetDatabase.Refresh();
                 }
                 
+                // 変換してファイルに書き込みます。
                 this.objWriter.SetValidReferencePoint(cityModel);
                 this.objWriter.Write(exportObjFilePath, cityModel, gmlFilePath);
-                
+
                 // 出力先が Assets フォルダ内なら、それをUnityに反映させます。
                 if (FilePathValidator.IsSubDirectoryOfAssets(exportObjFilePath))
                 {
                     string assetPath = FilePathValidator.FullPathToAssetsPath(exportObjFilePath);
                     AssetDatabase.ImportAsset(assetPath);
                     AssetDatabase.Refresh();
+                }
+                
+                // 変換後、3Dメッシュが1つもなければ、ファイルを削除して変換失敗とします。
+                // ただし、このチェックは出力パスが Assets フォルダ内である時のみ機能します。
+                if (FilePathValidator.IsSubDirectoryOfAssets(exportObjFilePath))
+                {
+                    string assetPath = FilePathValidator.FullPathToAssetsPath(exportObjFilePath);
+                    var assets = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+                    if (!assets.OfType<Mesh>().Any())
+                    {
+                        Debug.LogError($"No mesh found. Deleting output obj file.\ngml path = {gmlFilePath}");
+                        AssetDatabase.DeleteAsset(assetPath);
+                        return false;
+                    }
                 }
             }
             catch (FileLoadException e)
