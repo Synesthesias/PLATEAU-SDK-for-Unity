fc45e9ba287b61ab2a4a271593113de0e22eeaf9
linoal <1321932+linoal@users.noreply.github.com>
2022-06-14 21:48:59 +0900

    foldout追加

--- Editor/CityModelImportWindow/CityMapMetaDataEditor.cs
+++ Editor/CityModelImportWindow/CityMapMetaDataEditor.cs
@@ -13,6 +13,7 @@ namespace PlateauUnitySDK.Editor.CityModelImportWindow
     public class CityMapMetaDataEditor : UnityEditor.Editor
     {
         private bool foldOutIdGmlTable;
+        private bool foldOutReconvert;
         private CityModelImportConfigGUI importConfigGUI = new CityModelImportConfigGUI();
         public override void OnInspectorGUI()
         {
@@ -37,16 +38,21 @@ namespace PlateauUnitySDK.Editor.CityModelImportWindow
 
             EditorGUILayout.Space(10);
             
-            HeaderDrawer.Draw("変換時情報");
+            HeaderDrawer.Draw("再変換画面");
             HeaderDrawer.IncrementDepth();
+
+            this.foldOutReconvert = EditorGUILayout.Foldout(this.foldOutReconvert, "再変換");
+            if (this.foldOutReconvert)
+            {
+                this.importConfigGUI.Config = metaData.cityModelImportConfig;
+                var importConfig = this.importConfigGUI.Draw();
             
-            this.importConfigGUI.Config = metaData.cityModelImportConfig;
-            var importConfig = this.importConfigGUI.Draw();
-            
-            // if (PlateauEditorStyle.MainButton("再変換"))
-            // {
+                // if (PlateauEditorStyle.MainButton("再変換"))
+                // {
                 // OnConvertButtonPushed(importConfig);
-            // }
+                // }
+            }
+            
             // base.OnInspectorGUI();
         }
 
