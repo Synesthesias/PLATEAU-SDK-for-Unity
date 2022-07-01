158b5affb51a67c06c6474797ecc706f1f226d9a
linoal <1321932+linoal@users.noreply.github.com>
2022-07-01 22:09:46 +0900

    コメント追記

--- Tests/TestUtils/TestImporter.cs
+++ Tests/TestUtils/TestImporter.cs
@@ -6,6 +6,16 @@ namespace PLATEAU.Tests.TestUtils
 {
     internal static class TestImporter
     {
+        /// <summary>
+        /// テスト用のPlateau元データをインポートします。
+        /// </summary>
+        /// <param name="pathSet">テストに使うデータに関して、入出力のパスセットを指定します。</param>
+        /// <param name="metaData">インポートによって生成されたメタデータを出力します。</param>
+        /// <param name="additionalConfigFunc">
+        /// インポート設定は通常 MinimumConfig が利用されますが、この設定をベースに追加で
+        /// 設定を書き換えたい場合は Action{CityImportConfig} によって設定を書き換える処理を渡します。 
+        /// </param>
+        /// <returns></returns>
         public static int Import(ImportPathForTests pathSet, out CityMetaData metaData, Action<CityImportConfig> additionalConfigFunc)
         {
             var config = ImportConfigFactoryForTests.MinimumConfig(pathSet.SrcUdxFullPath, pathSet.OutputDirAssetsPath);
