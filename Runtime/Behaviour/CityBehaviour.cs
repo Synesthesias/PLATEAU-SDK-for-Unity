83a0d3fb9fef5679ecc7748d23953cfa94a4cf83
linoal <1321932+linoal@users.noreply.github.com>
2022-07-04 14:58:17 +0900

    Attributesを取得するサンプルを作成

--- Runtime/Behaviour/CityBehaviour.cs
+++ Runtime/Behaviour/CityBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using PLATEAU.CityGML;
 using PLATEAU.CityMeta;
+using PLATEAU.Util;
 using UnityEngine;
 
 namespace PLATEAU.Behaviour
@@ -11,7 +12,7 @@ namespace PLATEAU.Behaviour
     public class CityBehaviour : MonoBehaviour
     {
         [SerializeField] private CityMetaData cityMetaData;
-        private readonly SemanticsLoader loader = new SemanticsLoader();
+        private readonly CityModelLoader loader = new CityModelLoader();
 
         /// <summary> ゲームオブジェクトとgmlファイルを対応付けるために必要な <see cref="CityMeta.CityMetaData"/> です。 </summary>
         public CityMetaData CityMetaData
@@ -21,11 +22,11 @@ namespace PLATEAU.Behaviour
         }
 
         /// <summary>
-        /// ゲームオブジェクト名 → <see cref="CityGML.CityObject"/>
+        /// ゲームオブジェクト名 から <see cref="CityGML.CityObject"/> を返します。
         /// </summary>
         /// <param name="gameObjName">ゲームオブジェクト名</param>
         /// <returns>引数に対応する<see cref="CityGML.CityObject"/></returns>
-        public CityObject LoadSemantics(string gameObjName)
+        public CityObject LoadCityObject(string gameObjName)
         {
             return this.loader.Load(gameObjName, this.cityMetaData);
         }
