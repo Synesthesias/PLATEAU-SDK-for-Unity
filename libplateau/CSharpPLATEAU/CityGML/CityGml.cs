b4a44849b3abe332b5bafb9bf36e60aa783d7a7b
linoal <1321932+linoal@users.noreply.github.com>
2022-07-25 13:05:28 +0900

    dll更新

--- libplateau/CSharpPLATEAU/CityGML/CityGml.cs
+++ libplateau/CSharpPLATEAU/CityGML/CityGml.cs
@@ -2,7 +2,6 @@
 using System;
 using System.IO;
 using PLATEAU.Interop;
-using UnityEngine;
 
 namespace PLATEAU.CityGML
 {
@@ -15,7 +14,6 @@ namespace PLATEAU.CityGML
         /// <param name="parserParams">変換の設定です。</param>
         /// <param name="logCallbacks">ログを受け取るコールバックです。省略または null の場合は C# の標準出力にログを転送します。</param>
         /// <param name="logLevel">ログの詳細度です。</param>
-        /// <returns><see cref="CityModel"/>を返します。例外時はnullを返します。</returns>
         public static CityModel Load(
             string gmlPath, CitygmlParserParams parserParams,
             LogCallbacks logCallbacks = null,
@@ -31,21 +29,10 @@ namespace PLATEAU.CityGML
                 logLevel, logCallbacks.LogErrorFuncPtr, logCallbacks.LogWarnFuncPtr, logCallbacks.LogInfoFuncPtr);
             if (result == APIResult.ErrorLoadingCityGml)
             {
-                Debug.LogError(
+                throw new FileLoadException(
                     $"Loading gml failed.\nPlease check codelist xml files are located in (gmlFolder)/../../codelists\nAND gml file is located at {gmlPath}\nand ");
-                return null;
             }
-
-            try
-            {
-                DLLUtil.CheckDllError(result);
-            }
-            catch (Exception e)
-            {
-                Debug.LogError($"Loading gml failed. gmlPath = {gmlPath}\n{e}");
-                return null;
-            }
-
+            DLLUtil.CheckDllError(result);
             return new CityModel(cityModelHandle);
         }
     }
