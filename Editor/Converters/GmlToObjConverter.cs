2f7944419361b0f91782482661f38b8d5dd91cf4
linoal <1321932+linoal@users.noreply.github.com>
2022-08-03 18:14:01 +0900

    objファイルが複数回インポートされてしまう問題を修正

--- Editor/Converters/GmlToObjConverter.cs
+++ Editor/Converters/GmlToObjConverter.cs
@@ -145,17 +145,17 @@ namespace PLATEAU.Editor.Converters
                 exportedFilePaths = this.meshConverter.Convert(exportDirFullPath, gmlFilePath, cityModel, this.dllLogger);
                 
                 // 出力先が Assets フォルダ内なら、それをUnityに反映させます。
-                if (PathUtil.IsSubDirectoryOfAssets(exportDirFullPath))
-                {
-                    AssetDatabase.SaveAssets(); // ここで Save しておかないと、次の AssetDatabase.Refresh() のタイミングで CityMetadata のデータが壊れる厄介なバグが発生します。
-                    foreach (string objFullPath in objFullPaths)
-                    {
-                        string assetPath = PathUtil.FullPathToAssetsPath(objFullPath);
-                        AssetDatabase.ImportAsset(assetPath);
-                        AssetDatabase.Refresh();
-                    }
-                    
-                }
+                // if (PathUtil.IsSubDirectoryOfAssets(exportDirFullPath))
+                // {
+                //     AssetDatabase.SaveAssets(); // ここで Save しておかないと、次の AssetDatabase.Refresh() のタイミングで CityMetadata のデータが壊れる厄介なバグが発生します。
+                //     foreach (string objFullPath in objFullPaths)
+                //     {
+                //         string assetPath = PathUtil.FullPathToAssetsPath(objFullPath);
+                //         AssetDatabase.ImportAsset(assetPath);
+                //         AssetDatabase.Refresh();
+                //     }
+                //     
+                // }
             }
             catch (Exception e)
             {
