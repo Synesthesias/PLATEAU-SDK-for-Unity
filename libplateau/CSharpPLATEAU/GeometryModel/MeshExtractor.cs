4df5fd4292cb16e400d3396b3771f7b83b5f50a6
linoal <1321932+linoal@users.noreply.github.com>
2022-08-24 18:33:50 +0900

    コード整理

--- libplateau/CSharpPLATEAU/GeometryModel/MeshExtractor.cs
+++ libplateau/CSharpPLATEAU/GeometryModel/MeshExtractor.cs
@@ -44,41 +44,5 @@ namespace PLATEAU.GeometryModel
             DLLUtil.CheckDllError(result);
             return new Model(outModelPtr);
         }
-        
-        
-        // TODO 仕様変更にともない不要、あとで消す
-        // public Mesh[] GridMerge(
-        //     CityModel cityModel,
-        //     CityObjectType targetTypeMask,
-        //     int gridNumX,
-        //     int gridNumY,
-        //     DllLogger logger)
-        // {
-        //     var result = NativeMethods.plateau_mesh_extractor_grid_merge(
-        //         this.handle,
-        //         cityModel.Handle,
-        //         targetTypeMask,
-        //         gridNumX,
-        //         gridNumY,
-        //         out int numPolygons,
-        //         logger.Handle
-        //     );
-        //     DLLUtil.CheckDllError(result);
-        //     
-        //     var polygonPointers = new IntPtr[numPolygons];
-        //     var result2 = NativeMethods.plateau_mesh_extractor_get_last_result_of_grid_merge(
-        //         this.handle,
-        //         polygonPointers
-        //     );
-        //     DLLUtil.CheckDllError(result2);
-        //     
-        //     var retPolygons = new Mesh[numPolygons];
-        //     for (int i = 0; i < numPolygons; i++)
-        //     {
-        //         retPolygons[i] = new Mesh(polygonPointers[i]);
-        //     }
-        //
-        //     return retPolygons;
-        // }
     }
 }
