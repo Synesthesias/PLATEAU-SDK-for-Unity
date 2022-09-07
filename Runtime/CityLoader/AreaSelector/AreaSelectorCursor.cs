decfaede04dabbe0e57b1a0886800f9a2ab4991c
linoal <1321932+linoal@users.noreply.github.com>
2022-09-07 18:56:27 +0900

    同上

--- Runtime/CityLoader/AreaSelector/AreaSelectorCursor.cs
+++ Runtime/CityLoader/AreaSelector/AreaSelectorCursor.cs
@@ -6,6 +6,7 @@ using Vector3 = UnityEngine.Vector3;
 
 #if UNITY_EDITOR
 using UnityEditor;
+using UnityEngine.UI;
 #endif
 
 namespace PLATEAU.CityLoader.AreaSelector
@@ -16,7 +17,10 @@ namespace PLATEAU.CityLoader.AreaSelector
         // [SerializeField] private Vector2 areaMin = Vector2.zero;
         // [SerializeField] private Vector2 areaMax = Vector2.one * 100;
         private readonly Color cursorColor = Color.blue;
-        private const float boxUpperPos = 100f;
+        private const float boxUpperHeight = 30f;
+        private const float boxBottomHeight = 1f; // カーソルの線が地面と重なって隠れない程度の高さ
+        private const float boxCenterHeight = (boxUpperHeight + boxBottomHeight) / 2.0f;
+        private const float boxSizeY = boxUpperHeight - boxBottomHeight;
 
         // public Vector2 CenterPos
         // {
@@ -42,11 +46,11 @@ namespace PLATEAU.CityLoader.AreaSelector
             #endif
             var trans = transform;
             var prevPos = trans.position;
-            var nextPos = new Vector3(prevPos.x, boxUpperPos / 2.0f, prevPos.z);
+            var nextPos = new Vector3(prevPos.x, boxCenterHeight, prevPos.z);
             trans.position = nextPos;
             
             var prevLocalScale = trans.lossyScale;
-            var nextLocalScale = new Vector3(prevLocalScale.x, boxUpperPos, prevLocalScale.z);
+            var nextLocalScale = new Vector3(prevLocalScale.x, boxSizeY, prevLocalScale.z);
             trans.localScale = nextLocalScale;
         }
 
@@ -64,9 +68,7 @@ namespace PLATEAU.CityLoader.AreaSelector
         {
             var trans = transform;
             var centerPos = trans.position;
-            var size = trans.lossyScale;
-            // Vector3 centerPos3D = new Vector3(centerPos.x, boxUpperPos/2.0f, centerPos.y);
-            // Vector3 size3D = new Vector3(size.x, boxUpperPos, size.z);
+            var size = trans.localScale;
             Gizmos.DrawWireCube(centerPos, size);
         }
 
@@ -74,34 +76,46 @@ namespace PLATEAU.CityLoader.AreaSelector
         {
             var prevColor = Handles.color;
             Handles.color = this.cursorColor;
-            // Vector3 nextCenter3D = new Vector3(CenterPos.x, boxUpperPos, CenterPos.y);
             var trans = transform;
+            
+            CenterPointHandle(trans);
+            CornerPointHandle(trans);
+            
+            Handles.color = prevColor;
+        }
+
+        private static void CenterPointHandle(Transform trans)
+        {
             var pos = trans.position;
-            var nextCenter = new Vector3(pos.x, boxUpperPos, pos.z);
-            
+            var handlePos = new Vector3(pos.x, boxUpperHeight, pos.z);
             EditorGUI.BeginChangeCheck();
-            
-            nextCenter = Handles.Slider2D(nextCenter, Vector3.up, Vector3.forward, Vector3.right,
-                HandleUtility.GetHandleSize(nextCenter), Handles.CircleHandleCap, 15);
-        
-            nextCenter = Handles.Slider(nextCenter, Vector3.right);
-            nextCenter = Handles.Slider(nextCenter, Vector3.forward);
+
+            handlePos = Slider2D(handlePos);
+            handlePos = Handles.Slider(handlePos, Vector3.right);
+            handlePos = Handles.Slider(handlePos, Vector3.forward);
             
             if (EditorGUI.EndChangeCheck())
             {
-                trans.position = nextCenter;
+                trans.position = new Vector3(handlePos.x, boxCenterHeight, handlePos.z);
             }
-            
-            EditorGUI.BeginChangeCheck();
+        }
+
+        private static void CornerPointHandle(Transform trans)
+        {
+            var pos = trans.position;
             var size = trans.localScale;
             var prevPosMax = CalcAreaMax(pos, size);
-            var nextPosMax = Handles.Slider2D(prevPosMax, Vector3.up, Vector3.forward, Vector3.right, HandleUtility.GetHandleSize(prevPosMax),
-                Handles.CircleHandleCap, 15);
             var prevPosMin = CalcAreaMin(pos, size);
-            var minHandlePos = new Vector3(prevPosMin.x, boxUpperPos, prevPosMin.z);
-            var minHandlePosNext = Handles.Slider2D(minHandlePos, Vector3.up,Vector3.forward, Vector3.right, HandleUtility.GetHandleSize(prevPosMax),
-                Handles.CircleHandleCap, 15);
-            var nextPosMin = new Vector3(minHandlePosNext.x, 0, minHandlePosNext.z);
+            
+            EditorGUI.BeginChangeCheck();
+            
+            var nextPosMax = Slider2D(prevPosMax);
+            var minHandlePos = new Vector3(prevPosMin.x, boxUpperHeight, prevPosMin.z);
+            
+            
+            var minHandlePosNext = Slider2D(minHandlePos);
+            var nextPosMin = new Vector3(minHandlePosNext.x, boxBottomHeight, minHandlePosNext.z);
+            
             if (EditorGUI.EndChangeCheck())
             {
                 (nextPosMin, nextPosMax) = SwapMinMaxIfReversed(nextPosMin, nextPosMax);
@@ -109,11 +123,22 @@ namespace PLATEAU.CityLoader.AreaSelector
                 trans.position = nextCenter2;
                 trans.localScale = nextSize2;
             }
-            
-            
-            
-            Handles.color = prevColor;
         }
+
+        private static Vector3 Slider2D(Vector3 sliderPos)
+        {
+            return Handles.Slider2D(
+                sliderPos, Vector3.up, Vector3.forward,
+                Vector3.right, HandleUtility.GetHandleSize(sliderPos) * 0.5f,
+                Handles.SphereHandleCap, 15
+            );
+        }
+
+        private static void Slider2DHandleCap(int controlID, Vector3 position, Quaternion rotation, float size, EventType eventType)
+        {
+            Handles.DotHandleCap(controlID, position, rotation, size, eventType);
+        }
+        
         #endif
 
         private static Vector3 CalcAreaMax(Vector3 center, Vector3 size)
@@ -147,31 +172,5 @@ namespace PLATEAU.CityLoader.AreaSelector
             return (new Vector3(minX, minY, minZ), new Vector3(maxX, maxY, maxZ));
         }
     }
-
-    // #if UNITY_EDITOR
-    // [CustomEditor(typeof(AreaSelectorCursor))]
-    // public class AreaSelectorCursorEditor : Editor
-    // {
-    //     private readonly Color cursorColor = Color.blue;
-    //     private void OnSceneGUI()
-    //     {
-    //         var cursor = (AreaSelectorCursor)target;
-    //         var prevColor = Handles.color;
-    //         Handles.color = this.cursorColor;
-    //         
-    //         EditorGUI.BeginChangeCheck();
-    //         
-    //         cursor.cursorPos = Handles.Slider2D(cursor.cursorPos, Vector3.up, Vector3.forward, Vector3.right,
-    //             HandleUtility.GetHandleSize(cursor.cursorPos), Handles.CircleHandleCap, 15);
-    //
-    //         cursor.cursorPos = Handles.Slider(cursor.cursorPos, Vector3.right);
-    //
-    //         if (EditorGUI.EndChangeCheck())
-    //         {
-    //             Debug.Log("slider dragged.");
-    //         }
-    //         Handles.color = prevColor;
-    //     }
-    // }
-    // #endif
+    
 }
