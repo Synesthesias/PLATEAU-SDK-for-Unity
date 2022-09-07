b91442ccc1b3a3e0cf9684fbb2a031226ba3af59
linoal <1321932+linoal@users.noreply.github.com>
2022-09-07 18:12:08 +0900

    同上

--- Runtime/CityLoader/AreaSelector/AreaSelectorCursor.cs
+++ Runtime/CityLoader/AreaSelector/AreaSelectorCursor.cs
@@ -13,26 +13,26 @@ namespace PLATEAU.CityLoader.AreaSelector
     [ExecuteInEditMode]
     public class AreaSelectorCursor : MonoBehaviour
     {
-        [SerializeField] private Vector2 areaMin = Vector2.zero;
-        [SerializeField] private Vector2 areaMax = Vector2.one * 100;
+        // [SerializeField] private Vector2 areaMin = Vector2.zero;
+        // [SerializeField] private Vector2 areaMax = Vector2.one * 100;
         private readonly Color cursorColor = Color.blue;
         private const float boxUpperPos = 100f;
 
-        public Vector2 CenterPos
-        {
-            get => (this.areaMin + this.areaMax) / 2.0f;
-            set
-            {
-                var diff = value - CenterPos;
-                this.areaMin += diff / 2.0f;
-                this.areaMax += diff / 2.0f;
-            }
-        }
+        // public Vector2 CenterPos
+        // {
+        //     get => (this.areaMin + this.areaMax) / 2.0f;
+        //     set
+        //     {
+        //         var diff = value - CenterPos;
+        //         this.areaMin += diff / 2.0f;
+        //         this.areaMax += diff / 2.0f;
+        //     }
+        // }
 
-        public Vector2 Size
-        {
-            get => this.areaMax - this.areaMin;
-        }
+        // public Vector2 Size
+        // {
+        //     get => this.areaMax - this.areaMin;
+        // }
 
         private void Start()
         {
@@ -40,6 +40,14 @@ namespace PLATEAU.CityLoader.AreaSelector
             SceneView.duringSceneGui -= OnSceneGUI;
             SceneView.duringSceneGui += OnSceneGUI;
             #endif
+            var trans = transform;
+            var prevPos = trans.position;
+            var nextPos = new Vector3(prevPos.x, boxUpperPos / 2.0f, prevPos.z);
+            trans.position = nextPos;
+            
+            var prevLocalScale = trans.lossyScale;
+            var nextLocalScale = new Vector3(prevLocalScale.x, boxUpperPos, prevLocalScale.z);
+            trans.localScale = nextLocalScale;
         }
 
         private void OnDestroy()
@@ -54,33 +62,90 @@ namespace PLATEAU.CityLoader.AreaSelector
 
         private void OnDrawGizmos()
         {
-            Vector3 centerPos3D = new Vector3(CenterPos.x, boxUpperPos/2.0f, CenterPos.y);
-            Vector3 size3D = new Vector3(Size.x, boxUpperPos, Size.y);
-            Gizmos.DrawWireCube(centerPos3D, size3D);
+            var trans = transform;
+            var centerPos = trans.position;
+            var size = trans.lossyScale;
+            // Vector3 centerPos3D = new Vector3(centerPos.x, boxUpperPos/2.0f, centerPos.y);
+            // Vector3 size3D = new Vector3(size.x, boxUpperPos, size.z);
+            Gizmos.DrawWireCube(centerPos, size);
         }
 
         private void OnSceneGUI(SceneView _)
         {
             var prevColor = Handles.color;
             Handles.color = this.cursorColor;
-            Vector3 nextCenter3D = new Vector3(CenterPos.x, boxUpperPos, CenterPos.y);
-            
+            // Vector3 nextCenter3D = new Vector3(CenterPos.x, boxUpperPos, CenterPos.y);
+            var trans = transform;
+            var pos = trans.position;
+            var nextCenter = new Vector3(pos.x, boxUpperPos, pos.z);
             
             EditorGUI.BeginChangeCheck();
             
-            nextCenter3D = Handles.Slider2D(nextCenter3D, Vector3.up, Vector3.forward, Vector3.right,
-                HandleUtility.GetHandleSize(nextCenter3D), Handles.CircleHandleCap, 15);
+            nextCenter = Handles.Slider2D(nextCenter, Vector3.up, Vector3.forward, Vector3.right,
+                HandleUtility.GetHandleSize(nextCenter), Handles.CircleHandleCap, 15);
         
-            nextCenter3D = Handles.Slider(nextCenter3D, Vector3.right);
-            nextCenter3D = Handles.Slider(nextCenter3D, Vector3.forward);
+            nextCenter = Handles.Slider(nextCenter, Vector3.right);
+            nextCenter = Handles.Slider(nextCenter, Vector3.forward);
             
             if (EditorGUI.EndChangeCheck())
             {
-                CenterPos = new Vector2(nextCenter3D.x, nextCenter3D.z);
+                trans.position = nextCenter;
             }
+            
+            EditorGUI.BeginChangeCheck();
+            var size = trans.localScale;
+            var prevPosMax = CalcAreaMax(pos, size);
+            var nextPosMax = Handles.Slider2D(prevPosMax, Vector3.up, Vector3.forward, Vector3.right, HandleUtility.GetHandleSize(prevPosMax),
+                Handles.CircleHandleCap, 15);
+            var prevPosMin = CalcAreaMin(pos, size);
+            var minHandlePos = new Vector3(prevPosMin.x, boxUpperPos, prevPosMin.z);
+            var minHandlePosNext = Handles.Slider2D(minHandlePos, Vector3.up,Vector3.forward, Vector3.right, HandleUtility.GetHandleSize(prevPosMax),
+                Handles.CircleHandleCap, 15);
+            var nextPosMin = new Vector3(minHandlePosNext.x, 0, minHandlePosNext.z);
+            if (EditorGUI.EndChangeCheck())
+            {
+                (nextPosMin, nextPosMax) = SwapMinMaxIfReversed(nextPosMin, nextPosMax);
+                var (nextCenter2, nextSize2) = CalcTransFromArea(nextPosMax, nextPosMin);
+                trans.position = nextCenter2;
+                trans.localScale = nextSize2;
+            }
+            
+            
+            
             Handles.color = prevColor;
         }
         #endif
+
+        private static Vector3 CalcAreaMax(Vector3 center, Vector3 size)
+        {
+            return center + size / 2.0f;
+        }
+
+        private static Vector3 CalcAreaMin(Vector3 center, Vector3 size)
+        {
+            return center - size / 2.0f;
+        }
+
+        private static (Vector3 center, Vector3 size) CalcTransFromArea(Vector3 areaMax, Vector3 areaMin)
+        {
+            var center = (areaMax + areaMin) / 2.0f;
+            var size = areaMax - areaMin;
+            return (center, size);
+        }
+
+        private static (Vector3 min, Vector3 max) SwapMinMaxIfReversed(Vector3 min, Vector3 max)
+        {
+            var minX = min.x;
+            var minY = min.y;
+            var minZ = min.z;
+            var maxX = max.x;
+            var maxY = max.y;
+            var maxZ = max.z;
+            if (minX > maxX) (minX, maxX) = (maxX, minX);
+            if (minY > maxY) (minY, maxY) = (maxY, minY);
+            if (minZ > maxZ) (minZ, maxZ) = (maxZ, minZ);
+            return (new Vector3(minX, minY, minZ), new Vector3(maxX, maxY, maxZ));
+        }
     }
 
     // #if UNITY_EDITOR
