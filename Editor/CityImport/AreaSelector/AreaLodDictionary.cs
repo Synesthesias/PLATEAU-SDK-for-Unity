5847e7daac735d02b88ad4ef769909029af0f3ff
linoal <1321932+linoal@users.noreply.github.com>
2022-11-12 21:56:19 +0900

    地域メッシュコード内の対応LOD取得機能を実装中

---
+++ Editor/CityImport/AreaSelector/AreaLodDictionary.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PLATEAU.Udx;
+
+namespace PLATEAU.Editor.CityImport.AreaSelector
+{
+    public class AreaLodSearcher
+    {
+        // MeshCode <- (1対多) <- [ Package種, (多)LODs ]
+        private ConcurrentDictionary<MeshCode, ConcurrentBag<PackageLod>> data;
+        private string rootPath;
+
+        public AreaLodSearcher(string rootPath)
+        {
+            this.data = new ConcurrentDictionary<MeshCode, ConcurrentBag<PackageLod>>();
+            this.rootPath = rootPath;
+        }
+        
+        public class PackageLod
+        {
+            public PredefinedCityModelPackage Package { get; private set; }
+            public List<uint> Lods { get; private set; }
+
+            public PackageLod(PredefinedCityModelPackage package, IEnumerable<uint> lods)
+            {
+                Package = package;
+                Lods = lods.ToList();
+            }
+        }
+
+        public void AddLodsInMeshCode(MeshCode meshCode)
+        {
+            var packageLods = SearchLodsInMeshCodeInner(meshCode, rootPath);
+            foreach (var packageLod in packageLods)
+            {
+                this.data.AddOrUpdate(meshCode,
+                    _ => new ConcurrentBag<PackageLod> { packageLod },
+                    (_, bag) =>
+                    {
+                        bag.Add(packageLod);
+                        return bag;
+                    });
+            }
+        }
+
+        public IEnumerable<PackageLod> GetLodsInMeshCode(MeshCode meshCode)
+        {
+            if (this.data.TryGetValue(meshCode, out var packageLods))
+            {
+                return packageLods.ToArray();
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<PackageLod> SearchLodsInMeshCodeInner(MeshCode meshCode, string rootPath)
+        {
+            using var collection = UdxFileCollection
+                .Find(rootPath)
+                .FilterByMeshCodes(new[] { meshCode });
+
+            var packageLods = new List<PackageLod>();
+            foreach (PredefinedCityModelPackage package in Enum.GetValues(typeof(PredefinedCityModelPackage)))
+            {
+                var gmlPaths = collection.GetGmlFiles(package);
+                var lodSet = new SortedSet<uint>();
+                foreach (var gmlPath in gmlPaths)
+                {
+                    string fullPath = Path.GetFullPath(gmlPath);
+                    var lods = LodSearcher.SearchLodsInFile(fullPath);
+                    foreach (var lod in lods)
+                    {
+                        lodSet.Add(lod);
+                    }
+                }
+                packageLods.Add(new PackageLod(package, lodSet));
+            }
+
+            return packageLods;
+        } 
+    }
+}
