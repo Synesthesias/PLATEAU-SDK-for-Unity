6d9307d23005ecf96d5917c9e7bd4e5c4186d750
Wataru Yasuda <wataru.robo@gmail.com>
2023-01-31 12:17:06 +0900

    for iOS build

---
+++ Editor/BuildHelper.cs
@@ -0,0 +1,29 @@
+#if UNITY_IPHONE
+ 
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+ 
+public class BuildHelper
+{
+    [PostProcessBuildAttribute (1)]
+    public static void OnPostProcessBuild (BuildTarget target, string path)
+    {
+        if (target == BuildTarget.iOS)
+        {
+            var projectPath = PBXProject.GetPBXProjectPath (path);
+            var project = new PBXProject ();
+            project.ReadFromString (File.ReadAllText (projectPath));
+ 
+            var targetGUID = project.GetUnityFrameworkTargetGuid ();
+            project.SetBuildProperty (targetGUID, "ENABLE_BITCODE", "NO");
+            var iphoneGUID = project.GetUnityMainTargetGuid ();
+            project.SetBuildProperty (iphoneGUID, "ENABLE_BITCODE", "NO");
+ 
+            File.WriteAllText (projectPath, project.WriteToString ());
+        }
+    }
+}
+ 
+#endif
