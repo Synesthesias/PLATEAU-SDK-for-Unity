311daa3a6af5a8646b79d4e528abc0bce76a10cc
linoal <1321932+linoal@users.noreply.github.com>
2023-02-27 15:06:56 +0900

    リファクタ: 都市エクスポートの設定GUIとエクスポート処理について、ファイルフォーマットに応じて細かい場合分けがあったのを、インターフェイスを作って違いを吸収して場合分けを減らすリファクタ

---
+++ Editor/EditorWindow/PlateauWindow/MainTabGUI/ExportGUIParts/FbxModelExporter.cs
@@ -0,0 +1,20 @@
+﻿using PLATEAU.MeshWriter;
+using UnityEditor;
+
+namespace PLATEAU.Editor.EditorWindow.PlateauWindow.MainTabGUI.ExportGUIParts
+{
+    /// <summary>
+    /// 都市エクスポート設定のうち、ファイル形式にFBXを選択したときに固有のインポート設定です。
+    /// </summary>
+    internal class FbxExportConfig : PlateauModelExporter
+    {
+        public FbxFileFormat FbxFileFormat { get; set; } = FbxFileFormat.Binary;
+        
+        public void DrawConfigGUI()
+        {
+            FbxFileFormat = (FbxFileFormat)EditorGUILayout.EnumPopup("FBXフォーマット", FbxFileFormat);
+        }
+
+        
+    }
+}
---
+++ Editor/EditorWindow/PlateauWindow/MainTabGUI/ExportGUIParts/GltfModelExporter.cs
@@ -0,0 +1,20 @@
+﻿using PLATEAU.MeshWriter;
+using UnityEditor;
+
+namespace PLATEAU.Editor.EditorWindow.PlateauWindow.MainTabGUI.ExportGUIParts
+{
+    /// <summary>
+    /// Model(中間形式)をGLTFファイルにエクスポートします。
+    /// </summary>
+    internal class GltfExportConfig : PlateauModelExporter
+    {
+        public GltfFileFormat GltfFileFormat { get; set; } = GltfFileFormat.GLB;
+        
+        public void DrawConfigGUI()
+        {
+            GltfFileFormat = (GltfFileFormat)EditorGUILayout.EnumPopup("Gltfフォーマット", GltfFileFormat);
+        }
+
+        
+    }
+}
