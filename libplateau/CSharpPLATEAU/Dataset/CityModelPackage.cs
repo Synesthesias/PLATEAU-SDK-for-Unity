d1e228004efe5785bb0b37a63824ed11bbb4dbcb
COREI5\taka <seventhdim@gmail.com>
2023-06-09 14:38:02 +0900

    標準製品仕様書3.0パッケージ定義追加

--- libplateau/CSharpPLATEAU/Dataset/CityModelPackage.cs
+++ libplateau/CSharpPLATEAU/Dataset/CityModelPackage.cs
@@ -23,6 +23,30 @@ namespace PLATEAU.Dataset
         Relief = 1u << 6,
         //! 災害リスク
         DisasterRisk = 1u << 7,
+        //! 交通(鉄道) : rwy
+        Railway = 1u << 8,
+        //! 交通(航路) : wwy
+        Waterway = 1u << 9,
+        //! 水部 : wtr
+        WaterBody = 1u << 10,
+        //! 橋梁　 : brid
+        Bridge = 1u << 11,
+        //! 徒歩道 : trk
+        Track = 1u << 12,
+        //! 広場 : squr
+        Square = 1u << 13,
+        //! トンネル : tun
+        Tunnel = 1u << 14,
+        //! 地下埋設物 : unf
+        UndergroundFacility = 1u << 15,
+        //! 地下街 : ubld
+        UndergroundBuilding = 1u << 16,
+        //! 区域 : area 
+        Area = 1u << 17,
+        //! その他の構造物 : cons 
+        OtherConstruction = 1u << 18,
+        //! 汎用都市: gen
+        Generic = 1u << 19,
         //! その他
         Unknown = 1u << 31
     }
@@ -69,6 +93,42 @@ namespace PLATEAU.Dataset
                             break;
                         case PredefinedCityModelPackage.DisasterRisk:
                             names.Add( "災害リスク");
+                            break;
+                        case PredefinedCityModelPackage.Railway:
+                            names.Add("鉄道");
+                            break;
+                        case PredefinedCityModelPackage.Waterway:
+                            names.Add("航路");
+                            break;
+                        case PredefinedCityModelPackage.WaterBody:
+                            names.Add("水部");
+                            break;
+                        case PredefinedCityModelPackage.Bridge:
+                            names.Add("橋梁");
+                            break;
+                        case PredefinedCityModelPackage.Track:
+                            names.Add("徒歩道");
+                            break;
+                        case PredefinedCityModelPackage.Square:
+                            names.Add("広場");
+                            break;
+                        case PredefinedCityModelPackage.Tunnel:
+                            names.Add("トンネル");
+                            break;
+                        case PredefinedCityModelPackage.UndergroundFacility:
+                            names.Add("地下埋設物");
+                            break;
+                        case PredefinedCityModelPackage.UndergroundBuilding:
+                            names.Add("地下街");
+                            break;
+                        case PredefinedCityModelPackage.Area:
+                            names.Add("区域");
+                            break;
+                        case PredefinedCityModelPackage.OtherConstruction:
+                            names.Add("その他の構造物");
+                            break;
+                        case PredefinedCityModelPackage.Generic:
+                            names.Add("汎用都市");
                             break;
                         case PredefinedCityModelPackage.Unknown:
                             names.Add( "その他");
