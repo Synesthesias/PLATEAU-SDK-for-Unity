1e03452d744380f758d2d9ca41dab79878a3537a
Sacchy <mandw.entertainment@gmail.com>
2023-07-16 13:20:10 +0900

    範囲選択画面のアップデート
    ・左下パネルにLOD表示

--- Runtime/CityImport/AreaSelector/AreaSelectorGUI.cs
+++ Runtime/CityImport/AreaSelector/AreaSelectorGUI.cs
@@ -1,42 +1,66 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+using PLATEAU.Util;
 
 namespace PLATEAU.CityImport.AreaSelector
 {
     internal static class AreaSelectorGUI
     {
-        private static Rect windowRect;
-        private static readonly Rect initialWindowRect = new Rect(15f, 30f, 150f, 90f);
+        private static Rect currentAreaSelectionWindowRect;
+        private static Rect currentLodWindowRect;
+        private static readonly Rect AreaSelectionWindowRect = new(15f, 15f, 150f, 0f);
+        private static readonly Rect LodWindowRect = new(15f, Screen.height - 340f, 50f, 0f);
+        private static readonly string IconDirPath = PathUtil.SdkPathToAssetPath("Images/AreaSelect");
         private static AreaSelectorBehaviour areaSelector;
+        private static readonly List<Texture> LODIcons = new();
+        private static readonly GUIStyle IconStyle = new(EditorStyles.label) { fixedHeight = 30f, fixedWidth = 30f };
 
         public static void Enable(AreaSelectorBehaviour areaSelectorArg)
         {
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             SceneView.duringSceneGui += OnGUI;
-            windowRect = initialWindowRect;
+            currentAreaSelectionWindowRect = AreaSelectionWindowRect;
+            currentLodWindowRect = LodWindowRect;
             areaSelector = areaSelectorArg;
-            #endif
+            foreach (var iconName in new List<string>{"lod01.png", "lod02.png", "lod03.png", "lod04.png"})
+            {
+                var path = Path.Combine(IconDirPath, iconName).Replace('\\', '/');
+                var texture =  AssetDatabase.LoadAssetAtPath<Texture>(path);
+                if (texture == null)
+                {
+                    Debug.LogError($"Icon image file is not found : {path}");
+                    continue;
+                }
+                LODIcons.Add(texture);
+            }
+#endif
         }
 
         public static void Disable()
         {
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             SceneView.duringSceneGui -= OnGUI;
-            #endif
+#endif
         }
 
 #if UNITY_EDITOR
         private static void OnGUI(SceneView sceneView)
         {
             Handles.BeginGUI();
-            windowRect = GUILayout.Window(1, windowRect, DrawInsideWindow, "範囲選択");
+            currentAreaSelectionWindowRect = GUILayout.Window(1, currentAreaSelectionWindowRect, DrawAreaSelectionInsideWindow, "範囲選択");
+            currentLodWindowRect.y = sceneView.position.size.y - currentLodWindowRect.height * EditorGUIUtility.pixelsPerPoint;
+            currentLodWindowRect = GUILayout.Window(2, currentLodWindowRect, DrawLodInsideWindow, "LOD");
+            
             Handles.EndGUI();
         }
 #endif
 
-        private static void DrawInsideWindow(int id)
+        private static void DrawAreaSelectionInsideWindow(int id)
         {
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
+            GUILayout.Space(5f);
             if (GUILayout.Button("決定"))
             {
                 areaSelector.EndAreaSelection();
@@ -45,8 +69,26 @@ namespace PLATEAU.CityImport.AreaSelector
             {
                 areaSelector.CancelAreaSelection();
             }
-            GUI.DragWindow();
-            #endif
+            GUILayout.Space(5f);
+#endif
         }
+        
+        private static void DrawLodInsideWindow(int id)
+        {
+#if UNITY_EDITOR
+            GUILayout.Space(5f);
+            foreach (var lodIcon in LODIcons) 
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Box(lodIcon, IconStyle);
+                    GUILayout.FlexibleSpace();
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.Space(5f);
+#endif
+        }        
     }
 }
