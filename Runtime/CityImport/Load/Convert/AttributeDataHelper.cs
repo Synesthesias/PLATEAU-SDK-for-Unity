34c9dd6d032e0f09a0414dd2c0b2bf3fcf89fb8d
sevendev <taka@7jigen.net>
2023-07-20 14:26:56 +0900

    Feature/attr info as json string (#125)
    * tile download url changed to https
    package version updated 1.1.4
    
    * #付きTextureパス対応
    
    * 属性情報JSON標示用コンポーネント
    
    * CityObjectIndex -> cityObjectIndex
    parent処理追加
    
    * Raycastテスト
    AttributeDataHelper追加
    
    * 文字コード変更
    未使用using削除
    変数名修正
    Property->Serialize  Field
    
    * DeepCopy追加
    
    * json stringで管理するよう変更
    
    * CityObjectIndex取得処理追加
    
    * コンフリクト修正
    
    * コンパイルエラー修正、リネーム
    
    * 「MeshColliderをセットする」にチェックが入っている状態をデフォルトにした
    
    * 属性表示のリファクタ・サンプル実装
    
    * 地物情報を表示するためのDebugStringを実装中
    
    * DebugStringでAttributeを表示
    
    * クリックした位置の属性情報を取得するサンプルを実装
    
    * 微修正
    
    * 指摘点追加
    
    * AttributeMapのクラス化
    Refactoring
    
    * 勝又さんのプルリク修正に合わせ、属性取得サンプルが動作するようにしました
    
    * リファクタ：コードの余計な部分を削除
    
    * 属性表示のDebugStringをインデントさせるようにしました
    
    * CityParam の internal を private に変更しConveterの処理を変更
    
    ---------
    
    Co-authored-by: COREI5\taka <seventhdim@gmail.com>
    Co-authored-by: linoal <1321932+linoal@users.noreply.github.com>

---
+++ Runtime/CityImport/Load/Convert/AttributeDataHelper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using PLATEAU.CityGML;
+using PLATEAU.CityInfo;
+using PLATEAU.PolygonMesh;
+using UnityEngine;
+using CityObject = PLATEAU.CityInfo.CityObject;
+
+namespace PLATEAU.CityImport.Load.Convert
+{
+    /// <summary>
+    /// PLATEAU の属性情報を Unity の GameObjectで扱えるようにするためのHelperクラスです
+    /// </summary>
+    internal class AttributeDataHelper
+    {
+        private readonly CityModel cityModel;
+        private readonly MeshGranularity meshGranularity; 
+        private readonly List<CityObjectID> indexList = new();
+        private string id;
+        private CityObjectIndex index;
+        private string parant;
+
+        class CityObjectID
+        {
+            public CityObjectIndex Index;
+            public string AtomicID;
+            public string PrimaryID;
+        }
+
+        public AttributeDataHelper(CityModel cityModel, MeshGranularity granularity)
+        {
+            this.cityModel = cityModel;
+            meshGranularity = granularity;
+        }
+
+        public AttributeDataHelper(AttributeDataHelper attributeDataHelper) : this(attributeDataHelper.cityModel, attributeDataHelper.meshGranularity) { }
+
+        public void SetId(string id)
+        {
+            this.id = id;
+        }
+
+        /// <summary>
+        /// UV情報から地物IDを取得し保持します
+        /// </summary>
+        public void SetCityObjectList(CityObjectList cityObjectList)
+        {
+            indexList.Clear();
+            foreach (var key in cityObjectList.GetAllKeys())
+            {
+                var atomicGmlID = cityObjectList.GetAtomicID(key);
+                var primaryGmlID = cityObjectList.GetPrimaryID(key.PrimaryIndex);
+
+                if (meshGranularity == MeshGranularity.PerCityModelArea ||
+                    (meshGranularity == MeshGranularity.PerPrimaryFeatureObject && primaryGmlID == id))
+                        indexList.Add(new CityObjectID { Index = key, AtomicID = atomicGmlID, PrimaryID = primaryGmlID});
+                        
+                if (meshGranularity == MeshGranularity.PerAtomicFeatureObject && atomicGmlID == id)
+                    parant = primaryGmlID;
+            }
+            index =  cityObjectList.GetCityObjectIndex(id);         
+        }
+
+        /// <summary>
+        /// 各CityObjectの属性情報を取得してシリアライズ可能なデータに変換します
+        /// </summary>
+        public CityObject GetSerializableCityObject()
+        {
+            if (meshGranularity == MeshGranularity.PerCityModelArea)
+                return GetSerializableCityObjectForArea();
+
+            CityObject cityObjSer = new CityObject();
+
+            if (!string.IsNullOrEmpty(parant))
+                cityObjSer.parent = parant;
+
+            var cityObj = GetCityObjectById(this.id);
+            if (cityObj != null)
+            {
+                var ser = CityObjectSerializableConvert.FromCityGMLCityObject(cityObj, index);
+                foreach (var id in indexList)
+                {
+                    if (id.PrimaryID == id.AtomicID) continue;
+                    var childCityObj = GetCityObjectById(id.AtomicID);
+                    if (childCityObj == null) continue;
+                    ser.Children.Add(CityObjectSerializableConvert.FromCityGMLCityObject(childCityObj, id.Index));
+                }
+                cityObjSer.cityObjects.Add(ser);
+            }
+            return cityObjSer;
+        }
+
+        //地域単位結合モデルの場合のシリアライズ可能なデータへの変換です
+        public CityObject GetSerializableCityObjectForArea()
+        {
+            CityObject cityObjSer = new CityObject();
+            List<string> cityObjList = new List<string>();
+            Dictionary<string, List<CityObjectID>> chidrenMap = new Dictionary<string, List<CityObjectID>>();
+
+            foreach (var id in indexList)
+            {
+                if (string.IsNullOrEmpty(id.PrimaryID))
+                    cityObjList.Add(id.AtomicID);
+                else
+                {
+                    if (chidrenMap.ContainsKey(id.PrimaryID))
+                        chidrenMap[id.PrimaryID].Add(id);
+                    else
+                        chidrenMap.Add(id.PrimaryID, new List<CityObjectID> {id});
+                }
+            }
+
+            foreach (var id in indexList)
+            {
+                var cityObj = GetCityObjectById(id.AtomicID);
+                if (cityObj == null) continue;
+                var ser = CityObjectSerializableConvert.FromCityGMLCityObject(cityObj, id.Index);
+                if (!chidrenMap.ContainsKey(id.AtomicID)) continue;
+                var childrenId = chidrenMap[id.AtomicID];
+                foreach (var c in childrenId)
+                {
+                    if (c.PrimaryID == c.AtomicID) continue;
+                    var childCityObj = GetCityObjectById(c.AtomicID);
+                    if (childCityObj == null) continue;
+                    ser.Children.Add(CityObjectSerializableConvert.FromCityGMLCityObject(childCityObj, c.Index));
+                }
+                cityObjSer.cityObjects.Add(ser);
+            }
+            return cityObjSer;
+        }
+
+        public CityGML.CityObject GetCityObjectById(string id)
+        {
+            try
+            {
+                return cityModel.GetCityObjectById(id);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogWarning(ex.Message);
+            }
+            return null;
+        }
+    }
+}
