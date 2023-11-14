1574b4c7edebf469938b9aa4d683fa7195110c0a
linoal <1321932+linoal@users.noreply.github.com>
2023-11-14 17:29:32 +0900

    デフォルトテクスチャをエクスポートに対応させた

--- Runtime/CityExport/ModelConvert/UnityMeshToDllModelConverter.cs
+++ Runtime/CityExport/ModelConvert/UnityMeshToDllModelConverter.cs
@@ -5,6 +5,7 @@ using PLATEAU.CityInfo;
 using PLATEAU.Native;
 using PLATEAU.PolygonMesh;
 using PLATEAU.Util;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Assertions;
 using CityObjectList = PLATEAU.PolygonMesh.CityObjectList;
@@ -228,7 +229,19 @@ namespace PLATEAU.CityExport.ModelConvert
             if (mat == null) return "";
             var tex = mat.mainTexture;
             if (tex == null) return "";
+            
+            #if UNITY_EDITOR
+            // デフォルトマテリアルのテクスチャは、GetAssetPathでパスを取得できます。
+            string texAssetPath = AssetDatabase.GetAssetPath(tex);
+            if (texAssetPath != "")
+            {
+                return Path.GetFullPath(texAssetPath);
+            }
+            #endif
+            
+            // PLATEAUのテクスチャは、テクスチャ名がパスを表すこととしています。
             return Path.Combine(PathUtil.PLATEAUSrcFetchDir, tex.name);
+            
         }
     }
 }
