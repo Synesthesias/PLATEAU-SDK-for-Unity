f7c4a96a15153f15f2284ed1bf773ef52ec0db66
Linoal <1321932+linoal@users.noreply.github.com>
2024-01-29 18:35:58 +0900

    「重複する地物を非表示」機能において、LODごとに地物単位が違うケースに対応 (#236)
    * 「重複する地物を非表示」機能において、LODごとに単位が違うケースに対応中
    
    * リファクタ

--- Runtime/CityAdjust/ChangeActive/CityDuplicateProcessor.cs
+++ Runtime/CityAdjust/ChangeActive/CityDuplicateProcessor.cs
@@ -27,25 +27,84 @@ namespace PLATEAU.CityAdjust.ChangeActive
             {
                 var sortedLods = LODTransformsSorted(gmlTrans).ToArray();
                 // LODの高い方から
-                for (int l = sortedLods.Length - 1; l >= 0; l--)
+                for (int higherLodIndex = sortedLods.Length - 1; higherLodIndex >= 0; higherLodIndex--)
                 {
-                    var lodTrans = sortedLods[l];
-                    if (!lodTrans.gameObject.activeInHierarchy) continue;
-                    int objCount = lodTrans.childCount;
-                    for (int o = 0; o < objCount; o++)
+                    var higherLodTrans = sortedLods[higherLodIndex];
+                    if (!higherLodTrans.gameObject.activeInHierarchy) continue;
+                    int objCount = higherLodTrans.childCount;
+                    for (int higherObjIndex = 0; higherObjIndex < objCount; higherObjIndex++)
                     {
-                        var objTrans = lodTrans.GetChild(o);
-                        if (!objTrans.gameObject.activeInHierarchy) continue;
-                        if (IsAllChildrenDisabled(objTrans)) continue; // LOD2以上で、自身はActiveでも子はすべて非Activeになっているというケースに対応します。
-                        var objId = objTrans.name;
+                        var higherObjTrans = higherLodTrans.GetChild(higherObjIndex);
+                        if (!higherObjTrans.gameObject.activeInHierarchy) continue;
+                        if (IsAllChildrenDisabled(higherObjTrans)) continue; // LOD2以上で、自身はActiveでも子はすべて非Activeになっているというケースに対応します。
+                        var higherObjId = higherObjTrans.name;
+                        var higherGmlIds = GetCityObjs(higherObjTrans);
                         // LODの低いほうへ見て、重複があれば低いほうを非表示にします。
-                        for (int searchLod = l-1; searchLod >= 0; searchLod--)
+                        for (int lowerLodIndex = higherLodIndex-1; lowerLodIndex >= 0; lowerLodIndex--)
                         {
-                            var found = sortedLods[searchLod].transform.Find(objId);
-                            if (found != null)
+                            var lowerLodTrans = sortedLods[lowerLodIndex].transform;
+                            
+                            // 重複判定1 : 同名のゲームオブジェクトがあるか。
+                            // 例
+                            // LOD1 -> bldg_A
+                            // LOD2 -> bldg_A
+                            // というケースでは、LOD1 の bldg_A を非表示にする。
+                            var foundByName = lowerLodTrans.Find(higherObjId);
+                            if (foundByName != null)
                             {
-                                found.gameObject.SetActive(false);
+                                foundByName.gameObject.SetActive(false);
+                                continue;
                             }
+                            
+                            // 重複判定2 : LODの低いほうが単位が細かい状況で、LODの低いほうのゲームオブジェクトが高いほうのGML IDに含まれるケース
+                            // 例
+                            // LOD1 -> bldg_A, bldg_B
+                            // LOD2 -> group1
+                            // ただし、bldg_A が group1 の gmlID に含まれている
+                            // というケースでは、bldg_A を非表示にする。
+                            // インポート時にはこのケースはないが、分割結合機能を使うとこのケースはありうる。
+                            if (higherGmlIds.Count > 0)
+                            {
+                                foreach (var higherGmlId in higherGmlIds)
+                                {
+                                    var foundByGmlId = lowerLodTrans.Find(higherGmlId);
+                                    if (foundByGmlId != null)
+                                    {
+                                        foundByGmlId.gameObject.SetActive(false);
+                                    }
+                                }
+                                
+                            }
+                            
+                            // 重複判定3 : LODの低い方が単位が粗い状況で、LODの低いほうのGML IDを、高いほうがすべて含むケース
+                            // 例
+                            // LOD1 -> area1
+                            // LOD2 -> bldg_A, bldg_B
+                            // ただし、area1 の gmlID を列挙すると bldg_A, bldg_B となり、そのすべてが LOD2 に含まれている
+                            // というケースでは、area1を非表示にする。
+                            // インポート時にはこのケースはないが、分割結合機能を使うとこのケースはありうる。
+                            foreach (Transform lowerObjTrans in lowerLodTrans)
+                            {
+                                var lowerGmls = GetCityObjs(lowerObjTrans);
+                                if (lowerGmls.Count > 0)
+                                {
+                                    bool upperLodContainsAllLowerLodGmlId = true;
+                                    foreach (var lowerGml in lowerGmls)
+                                    {
+                                        if (higherLodTrans.Find(lowerGml) == null)
+                                        {
+                                            upperLodContainsAllLowerLodGmlId = false;
+                                            break;
+                                        }
+                                    }
+
+                                    if (upperLodContainsAllLowerLodGmlId)
+                                    {
+                                        lowerObjTrans.gameObject.SetActive(false);
+                                    }
+                                }
+                            }
+                            
                         }
                     }
                 }
@@ -56,6 +115,23 @@ namespace PLATEAU.CityAdjust.ChangeActive
         {
             var lods = PLATEAUInstancedCityModel.GetLodTransforms(gmlTrans);
             return lods.OrderBy(lod => lod.name);
+        }
+
+        private static List<string> GetCityObjs(Transform trans)
+        {
+            // 重複判定2,3向けのメソッドです。
+            var components = trans.GetComponentsInChildren<PLATEAUCityObjectGroup>();
+            var gmlIds = new List<string>();
+            // コンポーネントが見つからない場合は空配列とします。
+            if (components.Length == 0) return gmlIds;
+            foreach (var c in components)
+            {
+                // 下の式において、rootCityObjects の children までは見なくて良いです。
+                // なぜなら、それは最小地物であり、最小単位で disable したい状況は重複判定1で拾えており、重複判定2,3ではないからです。
+                gmlIds.AddRange(c.CityObjects.rootCityObjects.Select(cityObj => cityObj.GmlID));
+            }
+
+            return gmlIds;
         }
 
         /// <summary>
