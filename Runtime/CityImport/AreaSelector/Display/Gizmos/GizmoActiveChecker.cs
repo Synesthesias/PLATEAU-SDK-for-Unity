61aa3de14fa5f39d1dce3b01fb1d3589f819204f
Linoal <1321932+linoal@users.noreply.github.com>
2024-04-20 13:34:49 +0900

    微修正

--- Runtime/CityImport/AreaSelector/Display/Gizmos/GizmoActiveChecker.cs
+++ Runtime/CityImport/AreaSelector/Display/Gizmos/GizmoActiveChecker.cs
@@ -37,7 +37,7 @@ namespace PLATEAU.CityImport.AreaSelector.Display.Gizmos
             var centeredRect = new Rect((sceneView.position.width - 600) / 2, (sceneView.position.height - 120) / 2, 600,
                 120);
             GUI.Box(centeredRect,
-                "注意：\nギズモ表示がオフです。\n範囲を表示するために、シーンビューのギズモを表示切替ボタンを\nクリックしてオンにしてください。",
+                "注意：\nギズモ表示がオフです。\n範囲を表示するために、シーンビューのギズモ表示切替ボタンを\nクリックしてオンにしてください。",
                 style);
             Handles.EndGUI();
         }
