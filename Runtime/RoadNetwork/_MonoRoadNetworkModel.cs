a2cf48a067ae447cc431099427d431684f05024a
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-05-08 20:12:22 +0900

    道路ネットワーク手動編集機能を提供するエディタの実装途中 　基本的なパラメータ類を実装 　モード変更を実装 仮でMonoRoadNetworkModelを継承したクラスを作成

---
+++ Runtime/RoadNetwork/_MonoRoadNetworkModel.cs
@@ -0,0 +1,22 @@
+﻿using PLATEAU.RoadNetwork.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PLATEAU
+{
+    public class _MonoRoadNetworkModel : RoadNetworkObject
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
