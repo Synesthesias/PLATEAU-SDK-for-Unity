8835b088c4b452f20364925c70428749939bf33c
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-05-10 16:30:36 +0900

    テスト用スクリプトをTesterフォルダーに移動

--- Runtime/RoadNetwork/Tester/_MonoRoadNetworkModel.cs
+++ Runtime/RoadNetwork/Tester/_MonoRoadNetworkModel.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace PLATEAU
+namespace PLATEAU.RoadNetwork
 {
     public class _MonoRoadNetworkModel : RoadNetworkObject
     {
