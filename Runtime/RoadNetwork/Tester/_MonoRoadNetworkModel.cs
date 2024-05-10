4aa1097115e39f8bdb2aa5506ac8bfc9a861bf0d
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-05-10 16:56:36 +0900

    コメントの追加 道路ネットワークエディタを表示するボタンの位置を変更

--- Runtime/RoadNetwork/Tester/_MonoRoadNetworkModel.cs
+++ Runtime/RoadNetwork/Tester/_MonoRoadNetworkModel.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 namespace PLATEAU.RoadNetwork
 {
+    /// <summary>
+    /// 道路ネットワーク用のMonoBehaviour
+    /// 仮で作成したもの　
+    /// 道路ネットワークのテスト環境が整ってきたら削除予定
+    /// </summary>
     public class _MonoRoadNetworkModel : RoadNetworkObject
     {
         // Start is called before the first frame update
