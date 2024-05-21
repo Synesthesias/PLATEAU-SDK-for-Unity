65d9f618ac281ba8aecac2006361bcae1f8ad306
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-05-21 16:10:03 +0900

    道路ネットワークを所持したオブジェクトが設定された際にエディタの項目にも設定するように対応 エディタの未使用のID入力欄と表示ラベルを非表示に（使わないようなら削除予定）

--- Editor/RoadNetwork/RoadNetworkUIDoc.cs
+++ Editor/RoadNetwork/RoadNetworkUIDoc.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -213,6 +214,7 @@ namespace PLATEAU.Editor.RoadNetwork
 
         private EnumField modeSelector;
         private ScrollView parameterView;
+        private ObjectField objSelecter;
 
         private readonly IRoadNetworkEditingSystem system;
         private readonly RoadNetworkEditorAssets assets;
@@ -365,6 +367,9 @@ namespace PLATEAU.Editor.RoadNetwork
             modeSelector = editorRoot.Q<EnumField>("ModeSelector");
             Assert.IsNotNull(modeSelector);
 
+            objSelecter = editorRoot.Q<ObjectField>("EditingTargetSelecter");
+            Assert.IsNotNull(objSelecter);
+
             parameterView = editorRoot.Q<ScrollView>("ParameterView");
             Assert.IsNotNull(parameterView);
 
@@ -377,7 +382,7 @@ namespace PLATEAU.Editor.RoadNetwork
         {
             //　parameterの変更は必ずシステムを介して行う
 
-            system.OnChangedRoadNetworkObject += UpdateMode;
+            system.OnChangedRoadNetworkObject += SelectRoadNetwrokObject;
             system.OnChangedEditMode += UpdateMode;
 
             modeSelector.RegisterCallback<ChangeEvent<Enum>>((evt) =>
@@ -405,6 +410,12 @@ namespace PLATEAU.Editor.RoadNetwork
             var mode = system.CurrentEditMode;
             parameterView.Clear();
             ParamterLayoutSet[mode](assets, parameterView);
+        }
+
+        private void SelectRoadNetwrokObject(object _, EventArgs _1)
+        {
+            objSelecter.value = system.RoadNetworkObject;
+            UpdateMode(null, null);
         }
     }
 
