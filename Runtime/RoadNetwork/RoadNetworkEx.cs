8d35d8e2d7603b90cd743bd8de93e416051109ac
ren <mizurenkon@gmail.com>
2024-06-12 21:20:29 +0900

    update : RoadNetwork/RoadNetworkEx.csにConvertCityObjectAsync関数仮追加

--- Runtime/RoadNetwork/RoadNetworkEx.cs
+++ Runtime/RoadNetwork/RoadNetworkEx.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using PLATEAU.CityExport.ModelConvert;
+using PLATEAU.CityExport.ModelConvert.SubMeshConvert;
+using PLATEAU.CityImport.Import.Convert;
+using PLATEAU.GranularityConvert;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PLATEAU.RoadNetwork
 {
@@ -25,6 +30,36 @@ namespace PLATEAU.RoadNetwork
             self.Remove(lane);
             foreach (var l in self)
                 l.RemoveConnection(lane);
+        }
+
+        public static async Task ConvertCityObjectsAsync(GranularityConvertOptionUnity conf)
+        {
+            if (!conf.IsValid())
+            {
+                return;
+            }
+
+            // 属性情報を覚えておきます。
+            var attributes = GmlIdToSerializedCityObj.ComposeFrom(conf.SrcGameObjs);
+
+            // PLATEAUInstancedCityModel が含まれる場合、これもコピーしたいので覚えておきます。
+            var instancedCityModelDict = InstancedCityModelDict.ComposeFrom(conf.SrcGameObjs);
+
+            var unityMeshToDllSubMeshConverter = new UnityMeshToDllSubMeshWithGameMaterial();
+
+            // ゲームオブジェクトを共通ライブラリのModelに変換します。
+            using var srcModel = UnityMeshToDllModelConverter.Convert(
+                conf.SrcGameObjs,
+                unityMeshToDllSubMeshConverter,
+                true, // 非表示のゲームオブジェクトも対象に含めます。なぜなら、LOD0とLOD1のうちLOD1だけがActiveになっているという状況で、変換後もToolkitsのLOD機能を使えるようにするためです。
+                VertexConverterFactory.NoopConverter());
+
+            // 共通ライブラリの機能でモデルを分割・結合します。
+            var converter = new GranularityConverter();
+            using var dstModel = converter.Convert(srcModel, conf.NativeOption);
+            dstModel.GetRootNodeAt(0);
+
+
         }
     }
 }
