fb78de9db69a0d3a1bdca8c946520d5a92bb1f05
ren <mizurenkon@gmail.com>
2024-06-14 23:16:32 +0900

    fix : サンドボックス用に使っていた単体テストを削除

--- Tests/RoadNetworkTest/TypeUtilTest.cs
+++ Tests/RoadNetworkTest/TypeUtilTest.cs
@@ -28,51 +28,5 @@ namespace Tests.RoadNetworkTest
                 Assert.IsTrue(lineString[0] == ret.FirstOrDefault()?.Item2, "lineString == ret.FirstOrDefault()?.Item2");
             }
         }
-
-        [Test()]
-        public void GenericListMemberTest()
-        {
-            var a = new List<int>();
-            var type = a.GetType();
-            var args = type.GenericTypeArguments;
-            var genType = type.GetGenericTypeDefinition();
-
-            Assert.IsTrue(genType == typeof(List<>), "genType == typeof(List<>)");
-        }
-
-        [Test()]
-        public void SerializeFieldTest()
-        {
-            var a = new List<int>();
-            var type = typeof(RoadNetworkDataLink);
-            var flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
-            var properties = type.GetProperties(flags);
-            var fields = type.GetFields(flags);
-            var propAttributes = properties.Select(p => p.CustomAttributes.ToList()).ToList();
-            var fieldAttributes = fields.Select(p => p.CustomAttributes.ToList()).ToList();
-
-        }
-
-        [Test()]
-        public void ObjectKeyTableTest()
-        {
-            var table = new Dictionary<object, object>();
-
-            table[1] = 10;
-
-            object key = 1;
-            Console.WriteLine(table[key]);
-        }
-
-        [Test()]
-        public void GetPropertyFieldInfoTest()
-        {
-            var props = typeof(RoadNetworkLink).GetProperty(nameof(RoadNetworkLink.MainLanes));
-            var getter = props.GetGetMethod(true);
-            var memberInfo = (System.Reflection.MemberInfo)getter;
-            var fieldInfo = memberInfo as FieldInfo;
-
-            Console.WriteLine(fieldInfo.DeclaringType);
-        }
     }
 }
