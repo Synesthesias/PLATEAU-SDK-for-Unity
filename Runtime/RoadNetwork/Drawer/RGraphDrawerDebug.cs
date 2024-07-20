3d3af24aaeb6e13a00e9fd314dc898711fe829df
ren <mizurenkon@gmail.com>
2024-07-20 19:25:38 +0900

    update : RGraphDrawerDebug.cs追加

---
+++ Runtime/RoadNetwork/Drawer/RGraphDrawerDebug.cs
@@ -0,0 +1,23 @@
+﻿using PLATEAU.RoadNetwork.Graph;
+
+namespace PLATEAU.RoadNetwork.Drawer
+{
+    public class RGraphDrawerDebug
+    {
+        void Draw(RPolygon polygon)
+        {
+            // Draw polygon
+        }
+
+        public void Draw(RGraph graph)
+        {
+            foreach (var vertex in graph.Vertices)
+            {
+                foreach (var edge in vertex.Edges)
+                {
+                    // Draw edge
+                }
+            }
+        }
+    }
+}
