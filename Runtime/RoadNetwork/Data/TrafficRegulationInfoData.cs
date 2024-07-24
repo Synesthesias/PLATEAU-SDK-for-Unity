8000c89c9a6bb7bdcb7ff6c06f34057a0fbc682b
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-07-24 18:16:52 +0900

    エラー修正

--- Runtime/RoadNetwork/Data/TrafficRegulationInfoData.cs
+++ Runtime/RoadNetwork/Data/TrafficRegulationInfoData.cs
@@ -30,42 +30,42 @@ namespace PLATEAU.RoadNetwork.Data
     [System.Serializable]
     public class RnDataTrafficLightController : IPrimitiveData
     {
-        //// #TODO : 消える予定
-        //// 自分自身を表すId
+        ////// #TODO : 消える予定
+        ////// 自分自身を表すId
+        ////[field: SerializeField]
+        ////public RnID<TrafficLightController> MyId { get; set; }
+
         //[field: SerializeField]
-        //public RnID<TrafficLightController> MyId { get; set; }
+        //[RoadNetworkSerializeMember]
+        //public RnID<RnDataNode> Parent { get; set; }
 
-        [field: SerializeField]
-        [RoadNetworkSerializeMember]
-        public RnID<RnDataNode> Parent { get; set; }
+        //// 連結しているレーン(上流)
+        //[field: SerializeField]
+        //[RoadNetworkSerializeMember(nameof(RnLane.NextLanes))]
+        //public List<RnID<RnDataLane>> NextLanes { get; set; } = new List<RnID<RnDataLane>>();
 
-        // 連結しているレーン(上流)
-        [field: SerializeField]
-        [RoadNetworkSerializeMember(nameof(RnLane.NextLanes))]
-        public List<RnID<RnDataLane>> NextLanes { get; set; } = new List<RnID<RnDataLane>>();
+        //// 連結しているレーン(下流)
+        //[field: SerializeField]
+        //[RoadNetworkSerializeMember(nameof(RnLane.PrevLanes))]
+        //public List<RnID<RnDataLane>> PrevLanes { get; set; } = new List<RnID<RnDataLane>>();
 
-        // 連結しているレーン(下流)
-        [field: SerializeField]
-        [RoadNetworkSerializeMember(nameof(RnLane.PrevLanes))]
-        public List<RnID<RnDataLane>> PrevLanes { get; set; } = new List<RnID<RnDataLane>>();
+        //// 境界線(下流)
+        //[field: SerializeField]
+        //[RoadNetworkSerializeMember(nameof(RnLane.PrevBorder))]
+        //public RnID<RnDataWay> PrevBorder { get; set; }
 
-        // 境界線(下流)
-        [field: SerializeField]
-        [RoadNetworkSerializeMember(nameof(RnLane.PrevBorder))]
-        public RnID<RnDataWay> PrevBorder { get; set; }
+        //// 境界線(上流)
+        //[field: SerializeField]
+        //[RoadNetworkSerializeMember(nameof(RnLane.NextBorder))]
+        //public RnID<RnDataWay> NextBorder { get; set; }
 
-        // 境界線(上流)
-        [field: SerializeField]
-        [RoadNetworkSerializeMember(nameof(RnLane.NextBorder))]
-        public RnID<RnDataWay> NextBorder { get; set; }
+        //[field: SerializeField]
+        //[RoadNetworkSerializeMember(nameof(RnLane.LeftWay))]
+        //public RnID<RnDataWay> LeftWay { get; set; }
 
-        [field: SerializeField]
-        [RoadNetworkSerializeMember(nameof(RnLane.LeftWay))]
-        public RnID<RnDataWay> LeftWay { get; set; }
-
-        [field: SerializeField]
-        [RoadNetworkSerializeMember(nameof(RnLane.RightWay))]
-        public RnID<RnDataWay> RightWay { get; set; }
+        //[field: SerializeField]
+        //[RoadNetworkSerializeMember(nameof(RnLane.RightWay))]
+        //public RnID<RnDataWay> RightWay { get; set; }
 
     }
 
