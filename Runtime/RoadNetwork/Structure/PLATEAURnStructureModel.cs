17e8cc486903d629a7da3c40d5fdd41b74d3ecbf
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-09-30 20:37:57 +0900

    Revert "（マージ時には削除する機能）テストコード"
    This reverts commit ba95c3893c22a533de1f76bbda829c74533dc2a8.

--- Runtime/RoadNetwork/Structure/PLATEAURnStructureModel.cs
+++ Runtime/RoadNetwork/Structure/PLATEAURnStructureModel.cs
@@ -1,11 +1,8 @@
 ﻿using PLATEAU.RoadNetwork.Data;
 using PLATEAU.Util;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 namespace PLATEAU.RoadNetwork.Structure
 {
     public class PLATEAURnStructureModel
@@ -53,143 +50,6 @@ namespace PLATEAU.RoadNetwork.Structure
             RoadNetwork.Deserialize(storage);
             sw.Stop();
             DebugEx.Log($"S{GetType().Name} Deserialize({sw.ElapsedMilliseconds}[ms])");
-
-            // 信号制御器のテスト処理　マージ前に削除予定
-            TestSetter();
-            [Obsolete("テスト用　マージ前に削除予定")]
-            void TestSetter()
-            {
-                //// 設定先のストレージ
-                //var getter = GetRoadNetworkDataGetter();
-                //var storage = getter.;
-
-                // Setterの取得
-                var setter = GetRoadNetworkDataSetter();
-
-                // ID生成に必要な機能を作成
-                var roadIdGenrator = setter.CreateIdGenerator<Data.RnDataRoadBase>();
-                var controllerIdGenrator = setter.CreateIdGenerator<Data.RnDataTrafficLightController>();
-                var lightIdGenrator = setter.CreateIdGenerator<Data.RnDataTrafficLight>();
-                var patternIdGenrator = setter.CreateIdGenerator<Data.RnDataTrafficSignalPattern>();
-                var phaseIdGenrator = setter.CreateIdGenerator<Data.RnDataTrafficSignalPhase>();
-
-                // テストデータの作成
-                var controllers = new List<Data.RnDataTrafficLightController>();
-                var lights = new List<Data.RnDataTrafficLight>();
-                var patterns = new List<Data.RnDataTrafficSignalPattern>();
-                var phases = new List<Data.RnDataTrafficSignalPhase>();
-
-                // 親からデータを作成　子への参照はデータ生成直後に行う
-
-                // 信号制御器のデータ作成
-                var intersectionId = new RnID<RnDataRoadBase>(9, roadIdGenrator); // 適当な交差点ID
-                var controllerA = new Data.RnDataTrafficLightController()
-                {
-                    Parent = intersectionId,  // 道路のID
-                    OffsetTrafficLight = RnID<RnDataTrafficLight>.Undefind
-                };
-                controllers.Add(controllerA);
-                var controllerAId = new RnID<RnDataTrafficLightController>(0, controllerIdGenrator);
-
-                // 適当な道路ID
-                var nRoad = 3;
-                var roadIds = new List<RnID<RnDataRoadBase>>(nRoad);
-                for (int i = 0; i < nRoad; i++)
-                {
-                    roadIds.Add(new RnID<RnDataRoadBase>(i, roadIdGenrator));
-                }
-
-                // 信号機のデータ作成
-                var nLight = nRoad;
-                var roadIdEnum = roadIds.GetEnumerator();
-                for (int i = 0; i < nLight; i++)
-                {
-                    roadIdEnum.MoveNext();
-
-                    var light = new Data.RnDataTrafficLight()
-                    {
-                        Parent = controllerAId,
-                        RoadId = roadIdEnum.Current,
-                        LaneType = "Lane",
-                        Distance = 1.0f,
-                    };
-                    lights.Add(light);
-                }
-
-                // 信号制御器に信号機を紐づけ
-                controllerA.TrafficLights = new List<RnID<RnDataTrafficLight>>(nLight);
-                for (int i = 0; i < nLight; i++)
-                {
-                    controllerA.TrafficLights.Add(new RnID<RnDataTrafficLight>(i, lightIdGenrator));
-                }
-
-                // 信号パターンの作成
-                var nPattern = 2;
-                for (int i = 0; i < nPattern; i++)
-                {
-                    var pattern = new Data.RnDataTrafficSignalPattern()
-                    {
-                        Parent = controllerAId,
-                        OffsetSeconds = 10.0f,
-                        OffsetType = OffsetRelationType.Absolute,
-                        StartOffsets = new DateTime(2024, 1, 1)
-                    };
-                    patterns.Add(pattern);
-                }
-
-                // 信号制御器に信号パターンを紐づけ
-                controllerA.SignalPatterns = new List<RnID<RnDataTrafficSignalPattern>>(nPattern);
-                for (int i = 0; i < nPattern; i++)
-                {
-                    controllerA.SignalPatterns.Add(new RnID<RnDataTrafficSignalPattern>(i, patternIdGenrator));
-                }
-
-                // パターンごとにフェーズを作成する
-                UnityEngine.Debug.Assert(controllerA.SignalPatterns.Count == patterns.Count);
-                var patternIdCnt = 0;
-                var patternIds = controllerA.SignalPatterns;
-                foreach (var pattern in patterns)
-                {
-                    // 信号フェーズのデータ作成
-                    var nPhasePerPattern = 3;   //　1パターンに対していくつフェーズがあるか
-                    var phasesPerPattern = new List<Data.RnDataTrafficSignalPhase>(nPhasePerPattern);
-                    for (int i = 0; i < nPhasePerPattern; i++)
-                    {
-                        var phase = new Data.RnDataTrafficSignalPhase()
-                        {
-                            Parent = patternIds[patternIdCnt],
-                            Order = i,
-                            Split = 5.0f,
-                            EnterableVehicleTypeMask = (int)(VehicleType.Smarll),
-                            BlueRoadPairs = roadIds,
-                            YellowRoadPairs = new List<RnID<RnDataRoadBase>>(),
-                            RedRoadPairs = new List<RnID<RnDataRoadBase>>(),
-                        };
-                        phasesPerPattern.Add(phase);
-                    }
-                    phases.AddRange(phasesPerPattern);
-
-                    // 信号パターンに信号フェーズを紐づけ
-                    pattern.Phases = new List<RnID<RnDataTrafficSignalPhase>>(nPhasePerPattern);
-                    for (int i = 0; i < nPhasePerPattern; i++)
-                    {
-                        pattern.Phases.Add(
-                            new RnID<RnDataTrafficSignalPhase>(i + patternIdCnt * nPhasePerPattern, phaseIdGenrator));
-                    }
-
-                    patternIdCnt++;
-                }
-
-
-                // テストデータの設定
-                var testData = new RnTrafficLightDataSet(
-                    controllers, lights, patterns, phases);
-
-                var isSuc = setter.SetTrafficSignalLightController(in testData);
-                UnityEngine.Debug.Assert(isSuc);
-
-
-            }
         }
 
         /// <summary>
