6359f316775a037510ebbb00ccb2a88cfe50b0cc
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-09-30 16:46:28 +0900

    SetterにID生成機能を持ったクラス実装

--- Runtime/RoadNetwork/Data/RoadNetworkDataSetter.cs
+++ Runtime/RoadNetwork/Data/RoadNetworkDataSetter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static PLATEAU.RoadNetwork.Data.PrimitiveDataStorage;
 
 namespace PLATEAU.RoadNetwork.Data
 {
@@ -44,6 +45,56 @@ namespace PLATEAU.RoadNetwork.Data
             primStorage.TrafficSignalPhases.WriteNew(dataSet.SignalPhases.ToArray());
 
             return true;
+        }
+
+        /// <summary>
+        /// ID生成に必要な機能を提供する
+        /// 注意　現在は信号制御器関係のみあつあう
+        /// </summary>
+        /// <typeparam name="TPrimType"></typeparam>
+        /// <returns></returns>
+        public IRnIDGeneratable CreateIdGenerator<TPrimType>()
+            where TPrimType : IPrimitiveData
+        {
+            if (typeof(TPrimType) == typeof(RnDataRoadBase))
+            {
+                return new IDGeneratorProxy<RnDataRoadBase>(primStorage.RoadBases);
+            }
+            else if (typeof(TPrimType) == typeof(RnDataTrafficLightController))
+            {
+                return new IDGeneratorProxy<RnDataTrafficLightController>(primStorage.TrafficLightControllers);
+            }
+            else if (typeof(TPrimType) == typeof(RnDataTrafficLight))
+            {
+                return new IDGeneratorProxy<RnDataTrafficLight>(primStorage.TrafficLights);
+            }
+            else if (typeof(TPrimType) == typeof(RnDataTrafficSignalPattern))
+            {
+                return new IDGeneratorProxy<RnDataTrafficSignalPattern>(primStorage.TrafficSignalPatterns);
+            }
+            else if (typeof(TPrimType) == typeof(RnDataTrafficSignalPhase))
+            {
+                return new IDGeneratorProxy<RnDataTrafficSignalPhase>(primStorage.TrafficSignalPhases);
+            }
+
+            Debug.LogError("Invalid type");
+            return null;
+        }
+
+        /// <summary>
+        /// 代理でID生成を行うためのクラス
+        /// </summary>
+        /// <typeparam name="TPrimType"></typeparam>
+        private class IDGeneratorProxy<TPrimType> : IRnIDGeneratable
+            where TPrimType : IPrimitiveData
+        {
+            public IDGeneratorProxy(PrimitiveStorage<TPrimType> primStorage)
+            {
+                this.primStorage = primStorage;
+            }
+
+            private PrimitiveStorage<TPrimType> primStorage;
+
         }
 
         private PrimitiveDataStorage primStorage;
