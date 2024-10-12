ec3be925daaa8a3cb1a84d58e995aa579d3823a9
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-10-12 20:05:33 +0900

    RnRoadGroup内の歩道を集めたSideWalkGroupを作成 取得する関数の実装

--- Runtime/RoadNetwork/Structure/RnRoadGroup.cs
+++ Runtime/RoadNetwork/Structure/RnRoadGroup.cs
@@ -641,6 +641,32 @@ namespace PLATEAU.RoadNetwork.Structure
             }
         }
 
+        public IReadOnlyCollection<RnSideWalkGroup> GetSideWalkGroups()
+        {
+            var sideWalks = new List<RnSideWalk>();
+            foreach (var road in Roads)
+            {
+                sideWalks.AddRange(road.GetSideWalks());
+            }
+
+            var groups = new List<RnSideWalkGroup>();
+            foreach (var sideWalk in sideWalks)
+            {
+                var group = groups.FirstOrDefault(g => g.SideWalks.Contains(sideWalk));
+                if (group == null)
+                {
+                    group = new RnSideWalkGroup(new List<RnSideWalk> { sideWalk });
+                    groups.Add(group);
+                }
+                else
+                {
+                    ((List<RnSideWalk>)group.SideWalks).Add(sideWalk);
+                }
+            }
+
+            return groups;
+        }
+
         /// <summary>
         /// 左側レーン数を変更する
         /// </summary>
@@ -897,6 +923,18 @@ namespace PLATEAU.RoadNetwork.Structure
             }
 
             return null;
+        }
+
+        public class RnSideWalkGroup
+        {
+            public RnSideWalkGroup(List<RnSideWalk> sideWalks)
+            {
+                this.sideWalks = sideWalks;
+            }
+
+            public IReadOnlyCollection<RnSideWalk> SideWalks => sideWalks;
+
+            List<RnSideWalk> sideWalks;
         }
 
     }
