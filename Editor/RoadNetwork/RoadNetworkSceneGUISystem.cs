6d84747abae288cf87cb04df300c389385069be9
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-10-12 19:35:58 +0900

    歩道のポイントも詳細編集の対象に追加

--- Editor/RoadNetwork/RoadNetworkSceneGUISystem.cs
+++ Editor/RoadNetwork/RoadNetworkSceneGUISystem.cs
@@ -617,7 +617,7 @@ namespace PLATEAU.Editor.RoadNetwork
                             }
                         }
 
-                        foreach (var lane in road.MainLanes)
+                        foreach (var lane in road.AllLanes)
                         {
                             if (state.isDirtyTarget)
                             {
