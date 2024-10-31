bc6d5a4bddf230128601e14334dd3e85ae4c6dbf
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-10-31 18:37:38 +0900

    各タブの枠組みの実装 枠組みをインスタンス化する部分を実装 タブの制御フローを実装

---
+++ Editor/Window/Main/Tab/RoadGUIParts/RoadTrafficRulePanel.cs
@@ -0,0 +1,31 @@
+﻿using PLATEAU.Editor.Window.Main.Tab.RoadGuiParts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace PLATEAU.Editor.Window.Main.Tab.RoadGuiParts
+{
+    /// <summary>
+    /// RoadNetwork_TrafficRulePanel.uxmlのバインドや挙動の制御を行うクラス
+    /// </summary>
+    public class RoadTrafficRulePanel : RoadAdjustGuiPartBase
+    {
+        static readonly string name = "RoadNetwork_TrafficRulePanel";
+
+
+        public RoadTrafficRulePanel() : base(name)
+        {
+        }
+
+        public override void Init(VisualElement root)
+        {
+            base.Init(root);
+        }
+
+        public override void Terminate(VisualElement root)
+        {
+            base.Terminate(root);
+        }
+    }
+}
