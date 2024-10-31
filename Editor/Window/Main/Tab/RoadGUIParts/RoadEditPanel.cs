a9ce6c3fe35ba9b6f05f5fd41737a78f9023a659
Ronnel216 <26947803+Ronnel216@users.noreply.github.com>
2024-11-01 06:41:18 +0900

    最低限のボタン類のイベントの実装

--- Editor/Window/Main/Tab/RoadGUIParts/RoadEditPanel.cs
+++ Editor/Window/Main/Tab/RoadGUIParts/RoadEditPanel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using PLATEAU.RoadNetwork.Structure.Drawer;
+using PLATEAU.RoadNetwork.Structure;
+using PLATEAU.RoadNetwork.Tester;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -11,7 +14,7 @@ namespace PLATEAU.Editor.Window.Main.Tab.RoadGuiParts
     public class RoadEditPanel : RoadAdjustGuiPartBase
     {
         static readonly string name = "RoadNetwork_EditPanel";
-
+        GameObject selfGameObject = null;
 
         public RoadEditPanel() : base(name)
         {
@@ -20,6 +23,57 @@ namespace PLATEAU.Editor.Window.Main.Tab.RoadGuiParts
         public override void Init(VisualElement root)
         {
             base.Init(root);
+
+            var rnMdl = GameObject.FindObjectOfType<PLATEAURnStructureModel>();
+            if (rnMdl == null)
+            {
+                Debug.LogError("RoadNetworkStructureModel is not found.");
+                return;
+            }
+            selfGameObject = rnMdl.gameObject;
+
+            // 初期値の設定
+
+            // ボタン類の設定
+            var editModeToggle = self.Q<Toggle>("EditModeButton");
+            if (editModeToggle == null)
+            {
+                Debug.LogError("EditModeToggle is not found.");
+                return;
+            }
+            editModeToggle.RegisterCallback<ChangeEvent<bool>>((evt) =>
+            {
+                if (evt.newValue)
+                {
+
+                }
+                else
+                {
+
+                }
+            });
+
+            var applyRoadButton = self.Q<Button>("ApplyRoadButton");
+            if (applyRoadButton == null)
+            {
+                Debug.LogError("ApplyRoadButton is not found.");
+                return;
+            }
+            applyRoadButton.clicked += () =>
+            {
+
+            };
+
+            var applyIntersectionButton = self.Q<Button>("ApplyIntersectionButton");
+            if (applyIntersectionButton == null)
+            {
+                Debug.LogError("ApplyIntersectionButton is not found.");
+                return;
+            }
+            applyIntersectionButton.clicked += () =>
+            {
+
+            };
         }
 
         public override void Terminate(VisualElement root)
