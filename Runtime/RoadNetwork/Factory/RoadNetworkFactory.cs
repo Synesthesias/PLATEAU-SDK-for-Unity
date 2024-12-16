bbc6ef961d699fe11456f8a98235afe5267e537b
ren <mizurenkon@gmail.com>
2024-12-16 19:30:03 +0900

    fix : UnityEditorのusingをifdef でくくる

--- Runtime/RoadNetwork/Factory/RoadNetworkFactory.cs
+++ Runtime/RoadNetwork/Factory/RoadNetworkFactory.cs
@@ -9,7 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace PLATEAU.RoadNetwork.Factory
