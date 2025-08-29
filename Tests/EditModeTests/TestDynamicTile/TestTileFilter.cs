867ae725ec3ca4535e10c58d3ac5166585a14f7c
sevendev <taka@7jigen.net>
2025-08-29 17:09:19 +0900

    Test 追加

--- Tests/EditModeTests/TestDynamicTile/TestTileFilter.cs
+++ Tests/EditModeTests/TestDynamicTile/TestTileFilter.cs
@@ -3,6 +3,9 @@ using PLATEAU.Dataset;
 using PLATEAU.DynamicTile;
 using PLATEAU.Native;
 using PLATEAU.Texture;
+using UnityEngine.TestTools;
+using UnityEngine;
+using PLATEAU.CityGML;
 
 
 namespace PLATEAU.Tests.EditModeTests
@@ -20,12 +23,28 @@ namespace PLATEAU.Tests.EditModeTests
             AssertPackage("tile_zoom_9_grid_53393660_brid_6697_op", PredefinedCityModelPackage.Bridge);
 
             AssertPackage("tile_zoom_10_grid_08EE754_tran_6697_op", PredefinedCityModelPackage.Road); //　Grid Code
+
+            AssertPackage("tile_zoom_10_grid_53394507_aaaa_6697_op", PredefinedCityModelPackage.Unknown);
+
+            LogAssert.Expect(LogType.Error, "アドレスからパッケージ名が取得できませんでした。Address: wrong_format");
+            AssertPackage("wrong_format", PredefinedCityModelPackage.None);
+            LogAssert.NoUnexpectedReceived();
+
+            // Fail
+            AssertPackageFail("tile_zoom_9_grid_53393660_brid_6697_op", PredefinedCityModelPackage.Building);
+            AssertPackageFail("tile_zoom_10_grid_53394517_tran_6697_op", PredefinedCityModelPackage.Railway);
         }
 
         private void AssertPackage(string address, PredefinedCityModelPackage expected)
         {
             var pkg = PLATEAUDynamicTileFilter.GetPackage(address);
             Assert.AreEqual(expected, pkg);
+        }
+
+        private void AssertPackageFail(string address, PredefinedCityModelPackage expected)
+        {
+            var pkg = PLATEAUDynamicTileFilter.GetPackage(address);
+            Assert.AreNotEqual(expected, pkg);  
         }
 
     }
