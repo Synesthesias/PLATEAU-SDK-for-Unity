532c58c948876985d611e0312737a7262698dd8a
azukizuki <kazuaki.terabayashiazuki@gmail.com>
2025-11-09 02:05:36 +0900

    review

--- Editor/DynamicTile/MultiResolutionPrefabCreator.cs
+++ Editor/DynamicTile/MultiResolutionPrefabCreator.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using PLATEAU.Util;
 using System;
 using System.Collections.Generic;
--- Runtime/CityImport/Import/TileImporter.cs
+++ Runtime/CityImport/Import/TileImporter.cs
@@ -215,6 +215,7 @@ namespace PLATEAU.CityImport.Import
                 token?.ThrowIfCancellationRequested();
 
                 await ImportGmlParallel(fetchedGmlFiles, 10f, 30f, token); // GML読込
+                
                 await ImportTiles(11, 40f, 60f, token);
                 await ImportTiles(10, 60f, 80f, token);
                 await ImportTiles(9, 80f, 100f, token);
@@ -705,9 +706,10 @@ namespace PLATEAU.CityImport.Import
             }
             else if (zoomLevel == 9)
             {
+                //zoomLevel9のときはLOD1テクスチャを作成する関係でLOD1,2を抽出させる
                 meshExtractOptions.MeshGranularity = MeshGranularity.PerCityModelArea;
                 meshExtractOptions.GridCountOfSide = 1;
-                meshExtractOptions.HighestLodOnly = false; // 高精細メッシュのみを抽出
+                meshExtractOptions.HighestLodOnly = false;
                 meshExtractOptions.SetLODRange(1,2);
             }
 
